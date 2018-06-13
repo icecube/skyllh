3cecf6b21c342b31e9e018337be7bb57ce3f7b52
martwo <martin.wolf@tum.de>
2018-06-13 21:36:59 +0000

    Added the TimeScramblerMethod class to scramble data via a scrambled time and a defined transformation function for horizontal to equatorial system coordinate transformations.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@163285 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/scrambling.py
+++ skylab/core/scrambling.py
@@ -5,6 +5,7 @@ import abc
 import numpy as np
 
 from skylab.core.random import RandomStateService
+from skylab.core.time import TimeGenerator
 
 class DataScramblingMethod(object):
     """Base class (type) for implementing a data scrambling method.
@@ -31,9 +32,11 @@ class DataScramblingMethod(object):
 class RAScramblingMethod(DataScramblingMethod):
     """The RAScramblingMethod method performs right-ascention scrambling within
     a given RA range. By default it's (0, 2\pi).
+
+    Note: This alters only the ``ra`` values of the data!
     """
     def __init__(self, rss, ra_range=None):
-        """Initializes a new RAScrambling instance.
+        """Initializes a new RAScramblingMethod instance.
 
         Parameters
         ----------
@@ -78,6 +81,64 @@ class RAScramblingMethod(DataScramblingMethod):
     def scramble(self, data):
         data["ra"] = self.rss.random.uniform(*self.ra_range, size=data.size)
 
+class TimeScramblingMethod(DataScramblingMethod):
+    """The TimeScramblingMethod class provides a data scrambling method to
+    perform data coordinate scrambling based on a generated time. It draws a
+    random time from a time generator and transforms the horizontal (local)
+    coordinates into equatorial coordinates using a specified transformation
+    function.
+    """
+    def __init__(self, timegen, hor_to_equ_transform):
+        """Initializes a new time scramling method instance.
+
+        Parameters
+        ----------
+        timegen : TimeGenerator
+            The time generator that should be used to generate random MJD times.
+        hor_to_equ_transform : callable
+            The transformation function to transform coordinates from the
+            horizontal system into the equatorial system.
+
+            The call signature must be:
+
+                __call__(azi, zen, mjd)
+
+            The return signature must be: (ra, dec)
+
+        """
+        super(TimeScramblingMethod, self).__init__()
+
+        self.timegen = timegen
+        self.hor_to_equ_transform = hor_to_equ_transform
+
+    @property
+    def timegen(self):
+        """The TimeGenerator instance that should be used to generate random MJD
+        times.
+        """
+        return self._timegen
+    @timegen.setter
+    def timegen(self, timegen):
+        if(not isinstance(timegen, TimeGenerator)):
+            raise TypeError('The timegen property must be an instance of TimeGenerator!')
+        self._timegen = timegen
+
+    @property
+    def hor_to_equ_transform(self):
+        """The transformation function to transform coordinates from the
+        horizontal system into the equatorial system.
+        """
+        return self._hor_to_equ_transform
+    @hor_to_equ_transform.setter
+    def hor_to_equ_transform(self, transform):
+        if(not callable(transform)):
+            raise TypeError('The hor_to_equ_transform property must be a callable object!')
+        self._hor_to_equ_transform = transform
+
+    def scramble(self, data):
+        mjds = self.timegen.generate_times(data.size)
+        data['time'] = mjds
+        (data['ra'], data['dec']) = self.hor_to_equ_transform(data['azi'], data['zen'], mjds)
 
 class DataScrambler(object):
     def __init__(self, method):
