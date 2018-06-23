8d442628e9ae9a55a0db11a6e7c7ac43cb106c9d
martwo <martin.wolf@tum.de>
2018-06-23 12:23:26 +0000

    Implemented the ObjectCollection class for a collection of objects of a specific object type.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@163615 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/py.py
+++ skylab/core/py.py
@@ -1,8 +1,14 @@
+import copy
+
+def typename(t):
+    """Returns the name of the given type ``t``.
+    """
+    return t.__name__
 
 def classname(obj):
     """Returns the name of the class of the class instance ``obj``.
     """
-    return type(obj).__name__
+    return typename(type(obj))
 
 def issequence(obj):
     """Checks if the given object ``obj`` is a squence or not. The definition of
@@ -54,3 +60,125 @@ def isproperty(obj, name):
         True if the given attribute is of type property, False otherwise.
     """
     return isinstance(type(obj).__dict__[name], property)
+
+class ObjectCollection(object):
+    """This class provides a collection of objects of a specific type. Objects
+    can be added to the collection via the ``add`` method or can be removed
+    from the collection via the ``pop`` method. The objects of another object
+    collection can be added to this object collection via the ``add`` method as
+    well.
+    """
+    def __init__(self, obj_t):
+        if(not issubclass(obj_t, object)):
+            raise TypeError('The obj_t argument must be a subclass of object!')
+        self._obj_type = obj_t
+        self._objects = []
+
+    @property
+    def obj_type(self):
+        """(read-only) The object type.
+        """
+        return self._obj_type
+
+    @property
+    def objects(self):
+        """(read-only) The list of objects of this object collection.
+        All objects are of the same type as specified through the ``obj_type``
+        property.
+        """
+        return self._objects
+
+    def __len__(self):
+        """Returns the number of objects being in this object collection.
+        """
+        return len(self._objects)
+
+    def __getitem__(self, key):
+        return self._objects[key]
+
+    def __iter__(self):
+        return iter(self._objects)
+
+    def __add__(self, other):
+        """Implementation to support the operation ``oc = self + other``, where
+        ``self`` is this ObjectCollection object and ``other`` something useful
+        else. This creates a copy ``oc`` of ``self`` and adds ``other``
+        to ``oc``.
+
+        Parameters
+        ----------
+        other : obj_type | ObjectCollection of obj_type
+
+        Returns
+        -------
+        oc : ObjectCollection
+            The new ObjectCollection object with object from self and other.
+        """
+        oc = self.copy()
+        oc.add(other)
+        return oc
+
+    def __str__(self):
+        """Pretty string representation of this object collection.
+        """
+        return classname(self)+ ': ' + str(self._objects)
+
+    def copy(self):
+        """Creates a copy of this ObjectCollection. The objects of the
+        collection are not copied!
+        """
+        oc = ObjectCollection(self._obj_type)
+        oc._objects = copy.copy(self._objects)
+        return oc
+
+    def add(self, obj):
+        """Adds the given object to the collection.
+
+        Parameters
+        ----------
+        obj : obj_type | ObjectCollection of obj_type
+            An instance of ``obj_type`` that should be added to the collection.
+            If given an ObjectCollection for objects of type obj_type, it will
+            add all objects of the given collection to this collection.
+
+        Returns
+        -------
+        self : ObjectCollection
+            The instance of this ObjectCollection, in order to be able to chain
+            several ``add`` calls.
+        """
+        if(isinstance(obj, ObjectCollection)):
+            if(typename(obj.obj_type) != typename(self._obj_type)):
+                raise TypeError('Cannot add objects from ObjectCollection for objects of type "%s" to this ObjectCollection for objects of type "%s"!'%(typename(obj.obj_type), typename(self._obj_type)))
+            self._objects.extend(obj.objects)
+            return self
+
+        if(not isinstance(obj, self._obj_type)):
+            raise TypeError('The object of type "%s" cannot be added to the object collection for objects of type "%s"!'%(classname(obj), typename(self._obj_type)))
+
+        self._objects.append(obj)
+        return self
+    __iadd__ = add
+
+    def index(self, obj):
+        return self._objects.index(obj)
+
+    def pop(self, index=None):
+        """Removes and returns object at given index (default last).
+        Raises IndexError if the collection is empty or index is out of range.
+
+        Parameters
+        ----------
+        index : int | None
+            The index of the object to remove. If set to None, the index of the
+            last object is used.
+
+        Returns
+        -------
+        obj : obj_type
+            The removed object.
+        """
+        if(index is None):
+            index = len(self._objects)-1
+        obj = self._objects.pop(index)
+        return obj
