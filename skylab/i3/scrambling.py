067fdb712b5f390f0acdc09e94d1bace82f6e647
martwo <martin.wolf@tum.de>
2018-06-23 10:14:13 +0000

    Import from the right location.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@163613 16731396-06f5-0310-8873-f7f720988828

--- skylab/i3/scrambling.py
+++ skylab/i3/scrambling.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from skylab.core.scrambling import TimeScramblingMethod
-
-from i3skylab.coord import hor_to_equ_transform, azi_to_ra_transform
+from skylab.i3.coords import hor_to_equ_transform, azi_to_ra_transform
 
 class I3TimeScramblingMethod(TimeScramblingMethod):
     """The I3TimeScramblingMethod class provides a data scrambling method to
