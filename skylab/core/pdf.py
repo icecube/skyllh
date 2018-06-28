72d9ef820e7ec6e3c5ecf0671943c93750904e18
martwo <martin.wolf@tum.de>
2018-06-28 16:33:16 +0000

    Implemented base PDF class.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@163756 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/pdf.py
+++ skylab/core/pdf.py
@@ -1,10 +1,55 @@
-class NullPDF(object):
-    """The base class for all probability distribution functions (PDF) classes.
-    Its main purpose is to allow for type checking.
-    Any kind of PDF classes must be derived from this class. 
-    """
-    pass
+# -*- coding: utf-8 -*-
 
-class LH(object):
+import abc
+
+class PDF(object):
+    """The abstract base class for all probability distribution functions (PDF)
+    models.
+    All PDF model classes must be derived from this class.
     """
-    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        # Make sure that multiple inheritance can be used. This super call will
+        # invoke the __init__ method of a possible second inheritance.
+        super(PDF, self).__init__()
+
+    @abc.abstractmethod
+    def assert_is_valid_for_exp_data(self, data_exp):
+        """This abstract method is supposed to check if this PDF is valid for
+        all the given experimental data. This means, it needs to check if there
+        is a PDF value for each data event that will be used in the likelihood
+        evaluation. This is just a seatbelt. The method must raise a ValueError
+        if the PDF is not valid for the given experimental data.
+
+        Parameters
+        ----------
+        data_exp : numpy record ndarray
+            The array holding the experimental data to check for.
+
+        Errors
+        ------
+        ValueError
+            If the PDF is not valid for the given experimental data.
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_prob(self, events, params, *args, **kwargs):
+        """This abstract method is supposed to calculate the probability for
+        the specified events given the specified parameters.
+
+        Parameters
+        ----------
+        events : numpy record ndarray
+            The numpy record ndarray holding the data events for which the
+            probability should be calculated for. What data fields are required
+            is defined by the derived PDF class and depends on the application.
+        params : dict
+            The dictionary containing the parameters needed for the calculation.
+            What parameters these are is defined by derived PDF class and
+            depends on the application.
+        """
+        pass
+
+
