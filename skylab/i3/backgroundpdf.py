a7c6327d359f82ba86a225df60a3e217fec17474
martwo <martin.wolf@tum.de>
2018-07-02 17:01:08 +0000

    Implemented background energy PDF class.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@163829 16731396-06f5-0310-8873-f7f720988828

--- skylab/i3/backgroundpdf.py
+++ skylab/i3/backgroundpdf.py
@@ -5,9 +5,9 @@ import numpy as np
 import scipy.interpolate
 
 from skylab.core.analysis import BinningDefinition, UsesBinning
-from skylab.core.backgroundpdf import SpatialBackgroundPDF
+from skylab.core.pdf import SpatialPDF, EnergyPDF
 
-class I3SpatialBackgroundPDF(SpatialBackgroundPDF, UsesBinning):
+class I3SpatialBackgroundPDF(SpatialPDF, UsesBinning):
     """This is the base class for all IceCube specific spatial background PDF
     models. IceCube spatial background PDFs depend soley on the zenith angle,
     and hence, on the declination of the event.
@@ -32,7 +32,7 @@ class I3SpatialBackgroundPDF(SpatialBackgroundPDF, UsesBinning):
         """
         super(I3SpatialBackgroundPDF, self).__init__()
 
-        self.add_binning(sinDec_binning)
+        self.add_binning(sinDec_binning, 'sin_dec')
         self.spline_order_sinDec = spline_order_sinDec
 
         (h, bins) = np.histogram(data_sinDec,
@@ -76,8 +76,13 @@ class I3SpatialBackgroundPDF(SpatialBackgroundPDF, UsesBinning):
             need to exist:
             'dec' : float
                 The declination of the data event.
+
+        Errors
+        ------
+        ValueError
+            If some of the data is outside the sin(dec) binning range.
         """
-        sinDec_binning = self.get_binning('sinDec')
+        sinDec_binning = self.get_binning('sin_dec')
         exp_sinDec = np.sin(data_exp['dec'])
 
         # Check if all the data is within the binning range.
@@ -94,12 +99,18 @@ class I3SpatialBackgroundPDF(SpatialBackgroundPDF, UsesBinning):
         events : numpy record ndarray
             The array holding the event data. The following data fields must
             exist:
-            'sinDec' : float
+            'sin_dec' : float
                 The sin(declination) value of the event.
         params : None
             Unused interface parameter.
+
+        Returns
+        -------
+        prob : 1d ndarray
+            The spherical probability of each data event.
         """
-        return 0.5 / np.pi * np.exp(self._log_spline(events['sinDec']))
+        prob = 0.5 / np.pi * np.exp(self._log_spline(events['sin_dec']))
+        return prob
 
 class I3DataSpatialBackgroundPDF(I3SpatialBackgroundPDF):
     """This is the IceCube spatial background PDF, which gets constructed from
@@ -161,15 +172,217 @@ class I3MCSpatialBackgroundPDF(I3SpatialBackgroundPDF):
             spatial background PDF along the sin(dec) axis.
             The default is 2.
         """
+        if(not isinstance(data_mc, np.ndarray)):
+            raise TypeError('The data_mc argument must be of type numpy.ndarray!')
+
+        if(isinstance(mc_weight_field_names, str)):
+            mc_weight_field_names = [mc_weight_field_names]
+        if(not issequenceof(mc_weight_field_names, str)):
+            raise TypeError('The mc_weight_field_names argument must be of type str or a sequence of type str!')
+
         data_sinDec = np.sin(data_mc['dec'])
 
         # Calculate the event weights as the sum of all the given data fields
         # for each event.
         data_weights = np.zeros(data_mc.size, dtype=np.float64)
         for name in mc_weight_field_names:
+            if(name not in data_mc.dtype.names):
+                raise KeyError('The field "%s" does not exist in the MC data!'%(name))
             data_weights += data_mc[name]
 
         # Create the PDF using the base class.
         super(I3MCSpatialBackgroundPDF, self).__init__(
             data_sinDec, data_weights, sinDec_binning, spline_order_sinDec
+        )
+
+class I3EnergyPDF(EnergyPDF, UsesBinning):
+    """This is the base class for all IceCube specific energy PDF models.
+    IceCube energy PDFs depend soley on the energy and the
+    zenith angle, and hence, on the declination of the event.
+
+    The IceCube energy PDF is modeled as a 1d histogram in energy,
+    but for different sin(declination) bins, hence, stored as a 2d histogram.
+    """
+    def __init__(self, data_logE, data_sinDec, data_weights, logE_binning, sinDec_binning):
+        """Creates a new IceCube energy PDF object.
+
+        Parameters
+        ----------
+        data_logE : 1d ndarray
+            The array holding the log10(E) values of the events.
+        data_sinDec : 1d ndarray
+            The array holding the sin(dec) values of the events.
+        data_weights : 1d ndarray
+            The array holding the weight of each event used for histogramming.
+        logE_binning : BinningDefinition
+            The binning definition for the log(E) axis.
+        sinDec_binning : BinningDefinition
+            The binning definition for the sin(declination) axis.
+        """
+        super(I3EnergyBackgroundPDF, self).__init__()
+
+        self.add_binning(logE_binning, 'log_energy')
+        self.add_binning(sinDec_binning, 'sin_dec')
+
+        # Create a 2D histogram. We will do the normalization along the logE
+        # axis manually.
+        (h, bins) = np.histogram2d(data_logE, data_sinDec,
+            bins = [logE_binning.binedges, sinDec_binning.binedges],
+            weights = data_weights,
+            range = [logE_binning.range, sinDec_binning.range],
+            normed = False)
+
+        # Calculate the normalization for each sin(dec) bin, i.e. along the
+        # logE-axis (axis 0).
+        norms = np.sum(h, axis=(0,)) * np.diff(logE_binning.binedges)
+        h /= norms
+
+        self._hist_logE_sinDec = h
+
+    def assert_is_valid_for_exp_data(self, data_exp):
+        """Checks if this energy PDF is valid for all the given experimental
+        data.
+        It checks if all the data is within the logE and sin(dec) binning range.
+
+        Parameters
+        ----------
+        data_exp : numpy record ndarray
+            The array holding the experimental data. The following data fields
+            must exist:
+            'log_energy' : float
+                The logarithm of the energy value of the data event.
+            'dec' : float
+                The declination of the data event.
+
+        Errors
+        ------
+        ValueError
+            If some of the data is outside the logE or sin(dec) binning range.
+        """
+        logE_binning = self.get_binning('log_energy')
+        sinDec_binning = self.get_binning('sin_dec')
+
+        exp_logE = data_exp['log_energy']
+        exp_sinDec = np.sin(data_exp['dec'])
+
+        # Check if all the data is within the binning range.
+        if(logE_binning.any_data_out_of_binning_range(exp_logE)):
+            raise ValueError('Some data is outside the logE range (%.3f, %.3f)!'%(logE_binning.lower_edge, logE_binning.upper_edge))
+        if(sinDec_binning.any_data_out_of_binning_range(exp_sinDec)):
+            raise ValueError('Some data is outside the sin(dec) range (%.3f, %.3f)!'%(sinDec_binning.lower_edge, sinDec_binning.upper_edge))
+
+    def get_prob(self, events, params=None):
+        """Calculates the energy probability (in logE) of each event.
+
+        Parameters
+        ----------
+        events : numpy record ndarray
+            The array holding the event data. The following data fields must
+            exist:
+            'log_energy' : float
+                The logarithm of the energy value of the event.
+            'sinDec' : float
+                The sin(declination) value of the event.
+        params : None
+            Unused interface parameter.
+
+        Returns
+        -------
+        prob : 1d ndarray
+            The array with the energy probability for each event.
+        """
+        logE_binning = self.get_binning('log_energy')
+        sinDec_binning = self.get_binning('sin_dec')
+
+        logE_idx = np.digitize(events['log_energy'], logE_binning.binedges)
+        sinDec_idx = np.digitize(events['sin_dec'], sinDec_binning.binedges)
+
+        prob = self._hist_logE_sinDec[(logE_idx,sinDec_idx)]
+        return prob
+
+class I3DataEnergyBackgroundPDF(I3EnergyPDF):
+    """This is the IceCube energy background PDF, which gets constructed from
+    experimental data.
+    """
+    def __init__(self, data_exp, logE_binning, sinDec_binning):
+        """Constructs a new IceCube energy background PDF from experimental
+        data.
+
+        Parameters
+        ----------
+        data_exp : numpy record ndarray
+            The array holding the experimental data. The following data fields
+            must exist:
+            'log_energy' : float
+                The logarithm of the energy value of the data event.
+            'dec' : float
+                The declination of the data event.
+        logE_binning : BinningDefinition
+            The binning definition for the binning in log10(E).
+        sinDec_binning : BinningDefinition
+            The binning definition for the sin(declination).
+        """
+        if(not isinstance(data_exp, np.ndarray)):
+            raise TypeError('The data_exp argument must be of type numpy.ndarray!')
+
+        data_logE = data_exp['log_energy']
+        data_sinDec = np.sin(data_exp['dec'])
+        data_weights = np.ones((data_exp.size,))
+
+        # Create the PDF using the base class.
+        super(I3DataEnergyBackgroundPDF, self).__init__(
+            data_logE, data_sinDec, data_weights,
+            logE_binning, sinDec_binning
+        )
+
+class I3MCEnergyBackgroundPDF(I3EnergyPDF):
+    """This is the IceCube energy background PDF, which gets constructed from
+    monte-carlo data.
+    """
+    def __init__(self, data_mc, mc_weight_field_names, logE_binning, sinDec_binning):
+        """Constructs a new IceCube energy background PDF from monte-carlo
+        data.
+
+        Parameters
+        ----------
+        data_mc : numpy record ndarray
+            The array holding the monte-carlo data. The following data fields
+            must exist:
+            'log_energy' : float
+                The logarithm of the energy value of the data event.
+            'dec' : float
+                The declination of the data event.
+        mc_weight_field_names : str | list of str
+            The name or the list of names of the monte-carlo data fields, which
+            should be used as event weights. If a list is given, the weight
+            values of all the fields will be summed to construct the final event
+            weight.
+        logE_binning : BinningDefinition
+            The binning definition for the binning in log10(E).
+        sinDec_binning : BinningDefinition
+            The binning definition for the sin(declination).
+        """
+        if(not isinstance(data_mc, np.ndarray)):
+            raise TypeError('The data_mc argument must be of type numpy.ndarray!')
+
+        if(isinstance(mc_weight_field_names, str)):
+            mc_weight_field_names = [mc_weight_field_names]
+        if(not issequenceof(mc_weight_field_names, str)):
+            raise TypeError('The mc_weight_field_names argument must be of type str or a sequence of type str!')
+
+        data_logE = data_mc['log_energy']
+        data_sinDec = np.sin(data_mc['dec'])
+
+        # Calculate the event weights as the sum of all the given data fields
+        # for each event.
+        data_weights = np.zeros(data_mc.size, dtype=np.float64)
+        for name in mc_weight_field_names:
+            if(name not in data_mc.dtype.names):
+                raise KeyError('The field "%s" does not exist in the MC data!'%(name))
+            data_weights += data_mc[name]
+
+        # Create the PDF using the base class.
+        super(I3MCEnergyBackgroundPDF, self).__init__(
+            data_logE, data_sinDec, data_weights,
+            logE_binning, sinDec_binning
         )
