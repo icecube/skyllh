dc1a0c36e3b556730e7d586a694e0b4bef9baaff
martwo <martin.wolf@tum.de>
2018-07-18 16:01:12 +0000

    Rename PointSource classes into PointLikeSource classes.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@164166 16731396-06f5-0310-8873-f7f720988828

--- skylab/physics/source.py
+++ skylab/physics/source.py
@@ -8,6 +8,7 @@ source at a given position in the sky with a given flux model.
 from skylab.core.py import ObjectCollection
 from skylab.physics.flux import FluxModel
 
+
 class SourceModel(object):
     """The base class for all source models in Skylab.
     """
@@ -30,22 +31,30 @@ class SourceCollection(ObjectCollection):
     """This class describes a collection of sources. It can be used to group
     sources into a single object, for instance for a stacking analysis.
     """
-    def __init__(self, source_t=None):
+    def __init__(self, source_type=None, sources=None):
         """Creates a new source collection.
 
         Parameters
         ----------
-        source_t : type | None
+        source_type : type | None
             The type of the source. If set to None (default), SourceModel will
             be used.
+        sources : sequence of source_t instances | None
+            The sequence of sources this collection should be initalized with.
         """
         if(source_t is None):
             source_t = SourceModel
-        super(SourceCollection, self).__init__(obj_t=source_t)
+        super(SourceCollection, self).__init__(obj_type=source_t, obj_list=sources)
+
+    @property
+    def source_type(self):
+        """(read-only) The type of the source model.
+        """
+        return self.obj_type
 
     @property
     def sources(self):
-        """(read-only) The list of sources of type SourceModel.
+        """(read-only) The list of sources of type ``source_t``.
         """
         return self.objects
 
@@ -53,18 +62,20 @@ class Catalog(SourceCollection):
     """This class describes a catalog of sources. It is derived from
     SourceCollection. A catalog has a name.
     """
-    def __init__(self, name, source_t=None):
+    def __init__(self, name, source_type=None, sources=None):
         """Creates a new source catalog.
 
         Parameters
         ----------
         name : str
             The name of the catalog.
-        source_t : type | None
+        source_type : type | None
             The type of the source. If set to None (default), the default type
             defined by SourceCollection will be used.
+        sources : sequence of source_t | None
+            The sequence of sources this catalog should be initalized with.
         """
-        super(Catalog, self).__init__(source_t=source_t)
+        super(Catalog, self).__init__(source_type=source_type, sources=sources)
         self.name = name
 
     @property
@@ -78,12 +89,18 @@ class Catalog(SourceCollection):
             raise TypeError('The name property must be of type str!')
         self._name = name
 
-class PointSource(SourceModel):
-    """The PointSource class is a source model for a point source like object
-    in the sky at a given location with a given flux model.
+    def as_source_collection(self):
+        """Creates a SourceCollection object for this catalog and returns it.
+        """
+        source_collection = SourceCollection(source_t=self.source_t, sources=self.sources)
+
+class PointLikeSource(SourceModel):
+    """The PointLikeSource class is a source model for a point-like source
+    object in the sky at a given location (right-ascention and declination) with
+    a given flux model.
     """
     def __init__(self, ra, dec, fluxmodel):
-        super(PointSource, self).__init__(fluxmodel)
+        super(PointLikeSource, self).__init__(fluxmodel)
         self.ra = ra
         self.dec = dec
 
@@ -109,15 +126,38 @@ class PointSource(SourceModel):
             raise TypeError('The dec property must be of type float!')
         self._dec = v
 
-class PointSourceCatalog(Catalog):
-    """Describes a catalog of point sources.
+
+class PointLikeSourceCollection(SourceCollection):
+    """Describes a collection of point-like sources.
     """
-    def __init__(self, name):
+    def __init__(self, sources=None):
+        """Creates a new collection of PointLikeSource objects.
+
+        Parameters
+        ----------
+        sources : sequence of PointLikeSource instances | None
+            The sequence of PointLikeSource objects this collection should be
+            initalized with.
+        """
+        super(PointLikeSourceCollection, self).__init__(
+            source_type=PointLikeSource, sources=sources)
+
+
+class PointLikeSourceCatalog(Catalog):
+    """Describes a catalog of point-like sources. The difference to a
+    PointLikeSourceCollection is the additional properties of a catalog, e.g.
+    the name.
+    """
+    def __init__(self, name, sources=None):
         """Creates a new point source catalog of the given name.
 
         Parameters
         ----------
         name : str
-            The name of the point source catalog.
+            The name of the point-like source catalog.
+        sources : sequence of PointLikeSource instances | None
+            The sequence of PointLikeSource instances this catalog should be
+            initalized with.
         """
-        super(PointSourceCatalog, self).__init__(name, source_t=PointSource)
+        super(PointLikeSourceCatalog, self).__init__(
+            name=name, source_type=PointLikeSource, sources=sources)
