1a240343281c8792bf3d087ba93693d8c958b4b7
martwo <martin.wolf@tum.de>
2018-07-25 10:31:34 +0000

    Implemented the FitParameter class and the SourceFitParameterManager classes together with a unit test.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@164286 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/parameters.py
+++ skylab/core/parameters.py
@@ -5,7 +5,8 @@ import itertools
 import numpy as np
 from copy import deepcopy
 
-from skylab.core.py import ObjectCollection
+from skylab.physics.source import SourceModel, SourceCollection
+from skylab.core.py import ObjectCollection, issequence, float_cast
 
 def make_linear_parameter_grid_1d(name, low, high, delta):
     """Utility function to create a ParameterGrid object for a 1-dimensional
@@ -357,3 +358,200 @@ class ParabolaFitParameterInterpolationMethod(FitParameterManifoldGridInterpolat
         gradients = 2. * a * (x - x1) + b
 
         return (value, np.atleast_2d(gradients))
+
+
+class FitParameter(object):
+    """This class describes a single fit parameter. A fit parameter has a name,
+    a value range, an initial value, and a current value. The current value will
+    be updated in the fitting process.
+    """
+    def __init__(self, name, valmin, valmax, initial):
+        """Creates a new fit parameter object.
+
+        Parameters
+        ----------
+        name : str
+            The name of the fit parameter.
+        valmin : float
+            The minimal bound value of the fit parameter.
+        valmax : float
+            The maximal bound value of the fit parameter.
+        initial : float
+            The (initial) value (guess) of the parameter, which will be used as
+            start point for the fitting procedure.
+        """
+        self.name = name
+        self.valmin = valmin
+        self.valmax = valmax
+        self.initial = initial
+
+        self.value = self.initial
+
+    @property
+    def name(self):
+        """The name of the fit parameter.
+        """
+        return self._name
+    @name.setter
+    def name(self, name):
+        if(not isinstance(name, str)):
+            raise TypeError('The name property must be of type str!')
+        self._name = name
+
+    @property
+    def valmin(self):
+        """The minimal bound value of the fit parameter.
+        """
+        return self._valmin
+    @valmin.setter
+    def valmin(self, v):
+        v = float_cast(v, 'The valmin property must castable to type float!')
+        self._valmin = v
+
+    @property
+    def valmax(self):
+        """The maximal bound value of the fit parameter.
+        """
+        return self._valmax
+    @valmax.setter
+    def valmax(self, v):
+        v = float_cast(v, 'The valmax property must be castable to type float!')
+        self._valmax = v
+
+    @property
+    def initial(self):
+        """The initial value of the fit parameter.
+        """
+        return self._initial
+    @initial.setter
+    def initial(self, v):
+        v = float_cast(v, 'The initial property must be castable to type float!')
+        self._initial = v
+
+
+class SourceFitParameterManager(object):
+    """This class provides the functionality to manage the fit parameters of the
+    sources. It provides a translator functionality to translate a fit
+    parameter to a source model parameter of a source.
+    Sometimes it's necessary to define a fit parameter, which relates to a
+    source model parameter for a set of sources, while another fit parameter
+    relates to the same source model parameter, but for another set of sources.
+
+    At construction time this manager takes the collection of sources. Each
+    source gets an index, which is defined as the position of the source within
+    the collection.
+    """
+    def __init__(self, sources):
+        """Constructs a new source fit parameter manager.
+
+        Parameters
+        ----------
+        sources : SourceCollection | SourceModel
+            The instance of a SourceCollection defining the list of sources.
+            If a single source model is specified a SourceCollection object with
+            this source will be constructed automatically.
+
+        """
+        self.sources = sources
+
+        # Define the list of fit parameters and the list of source parameter
+        # names, which map to the fit parameters.
+
+        # Define the (N_fitparams,)-shaped numpy array of FitParameter objects.
+        self._fit_params = np.empty((0,), dtype=np.object)
+        # Define the (N_fitparams,)-shaped numpy array of str objects.
+        self._src_param_names = np.empty((0,), dtype=np.object)
+        # (N_fitparams, N_sources) shaped boolean ndarray defining what fit
+        # parameter applies to which source.
+        self._fit_param_2_src_mask = np.zeros((0, len(self.sources)), dtype=np.bool)
+
+    @property
+    def sources(self):
+        """The SourceCollection defining the sources.
+        """
+        return self._sources
+    @sources.setter
+    def sources(self, obj):
+        obj = SourceCollection.cast(obj, 'The sources property must be castable to an instance of SourceCollection!')
+        self._sources = obj
+
+    @property
+    def initials(self):
+        """(read-only) The 1D ndarray holding the initial values of all the fit
+        parameters.
+        """
+        return np.array([ fit_param.initial
+                         for fit_param in self._fit_params ], dtype=np.float)
+
+    def def_fit_parameter(self, fit_param, src_param_name=None, sources=None):
+        """Defines a new fit parameter that maps to a given source parameter
+        for a list of sources. If no list of sources is given, it maps to all
+        sources.
+
+        Parameters
+        ----------
+        fit_param : FitParameter
+            The FitParameter instance defining the fit parameter.
+        src_param_name : str | None
+            The name of the source parameter. It must match the name of a source
+            model property. If set to None (default) the name of the fit
+            parameter will be used.
+        sources : SourceCollection | None
+            The instance of SourceCollection with the sources for which the fit
+            parameter applies. If None (the default) is specified, the fit
+            parameter will apply to all sources.
+        """
+        if(not isinstance(fit_param, FitParameter)):
+            raise TypeError('The fit_param argument must be an instance of FitParameter!')
+
+        if(not isinstance(src_param_name, str)):
+            raise TypeError('The src_param_name argument must be of type str!')
+
+        if(sources is None):
+            sources = self.sources
+        sources = SourceCollection.cast(sources,
+            'The sources argument must be castable to an instance of SourceCollection!')
+
+        # Append the fit parameter and source parameter name to the internal
+        # arrays.
+        self._fit_params = np.concatenate((self._fit_params, [fit_param]))
+        self._src_param_names = np.concatenate((self._src_param_names, [src_param_name]))
+
+        # Get the list of source indices for which the fit parameter applies.
+        mask = np.zeros((len(self.sources),), dtype=np.bool)
+        for ((idx,src), applied_src) in itertools.product(enumerate(self.sources), sources):
+            if(applied_src.id == src.id):
+                mask[idx] = True
+        self._fit_param_2_src_mask = np.vstack((self._fit_param_2_src_mask, mask))
+
+    def get_source_fit_params(self, src_idx, fit_param_values):
+        """Constructs a dictionary with the source parameters that are beeing
+        fitted. As values the given global fit parameter values will be used.
+        Hence, this method translates the global fit parameter values into the
+        source parameters.
+
+        Parameters
+        ----------
+        src_idx : int
+            The index of the source for which the parameters should get
+            retieved.
+        fit_param_values : 1D ndarray
+            The array holding the current global fit parameter values.
+
+        Returns
+        -------
+        src_fitparams : dict
+            The dictionary holding the translated source parameters that are
+            beeing fitted.
+        """
+        # Get the mask of global fit parameters that apply to the requested
+        # source.
+        fp_mask = self._fit_param_2_src_mask[:,src_idx]
+
+        # Get the source parameter names.
+        src_param_names = self._src_param_names[fp_mask]
+        src_param_values = fit_param_values[fp_mask]
+
+        src_fitparams = dict(zip(src_param_names, src_param_values))
+
+        return src_fitparams
--- tests/core/test_parameters.py
+++ tests/core/test_parameters.py
@@ -3,8 +3,6 @@
 import numpy as np
 import unittest
 
-from skylab.core.parameters import make_linear_parameter_grid_1d, ParameterGridSet
-
 def isAlmostEqual(a, b, decimals=9):
     a = np.atleast_1d(a)
     b = np.atleast_1d(b)
@@ -15,6 +13,8 @@ class TestParameters(unittest.TestCase):
         """Test the arameter_permutation_dict_list method of the
         ParameterGridSet class.
         """
+        from skylab.core.parameters import make_linear_parameter_grid_1d, ParameterGridSet
+
         GAMMA_GRID = [ 1. ,  1.1,  1.2,  1.3,  1.4,  1.5,  1.6,  1.7,  1.8,  1.9,  2. ]
         ECUT_GRID = [ 9., 9.1 ]
 
@@ -36,7 +36,38 @@ class TestParameters(unittest.TestCase):
         perm_dict_list = paramgridset.parameter_permutation_dict_list
         self.assertTrue(isAlmostEqual( [ d['Ecut'] for d in perm_dict_list ], ECUT_GRID*len(GAMMA_GRID)))
 
+    def test_SourceFitParameterManager(self):
+        from skylab.physics.flux import PowerLawFlux
+        from skylab.physics.source import PointLikeSource
+        from skylab.core.parameters import SourceFitParameterManager, FitParameter
 
+        # Define the flux model.
+        fluxmodel = PowerLawFlux(1, 1e3, -2)
+
+        # Define a list of point-like sources.
+        sources = [
+            PointLikeSource(np.deg2rad(120), np.deg2rad(-23), fluxmodel),
+            PointLikeSource(np.deg2rad(266), np.deg2rad(61), fluxmodel),
+        ]
+
+        # Define the fit parameters 'gamma1' and 'gamma2' which map to the
+        # 'gamma' source parameter of the first and second source, respectively.
+        sfpm = SourceFitParameterManager(sources)
+        sfpm.def_fit_parameter(FitParameter('gamma1', 1, 4, 2.0), 'gamma', sources[0])
+        sfpm.def_fit_parameter(FitParameter('gamma2', 1, 4, 2.1), 'gamma', sources[1])
+
+        # Check the initial values.
+        self.assertTrue(np.all(sfpm.initials == np.array([2.0, 2.1])))
+
+        # Get the source parameters for the first source (feed it with the
+        # initials).
+        fitparams = sfpm.get_source_fit_params(0, sfpm.initials)
+        self.assertEqual(fitparams, {'gamma': 2.0})
+
+        # Get the source parameters for the second source (feed it with the
+        # initials).
+        fitparams = sfpm.get_source_fit_params(1, sfpm.initials)
+        self.assertEqual(fitparams, {'gamma': 2.1})
 
 if(__name__ == '__main__'):
     unittest.main()
