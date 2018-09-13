3efe576d01ba13a78eb2b768f4998cd14eb2e2bf
martwo <martin.wolf@tum.de>
2018-09-13 10:01:40 +0000

    Use the source hypo group manager as source list resource for the event selection method feature. This makes it easier to change sources, e.g. when fitting the source location.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@165442 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/optimize.py
+++ skylab/core/optimize.py
@@ -3,7 +3,10 @@
 import abc
 import numpy as np
 
-from skylab.core.py import float_cast
+from skylab.core.py import float_cast, issequenceof
+from skylab.core.source_hypothesis import SourceHypoGroupManager
+from skylab.physics.source import SourceModel
+
 
 class EventSelectionMethod(object):
     """This is the abstract base class for all event selection method classes.
@@ -13,8 +16,62 @@ class EventSelectionMethod(object):
     """
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
+    def __init__(self, src_hypo_group_manager):
+        """Creates a new event selection method instance.
+
+        Parameters
+        ----------
+        src_hypo_group_manager : SourceHypoGroupManager instance
+            The instance of SourceHypoGroupManager that defines the list of
+            sources, i.e. the list of SourceModel instances.
+        """
         super(EventSelectionMethod, self).__init__()
+
+        self.src_hypo_group_manager = src_hypo_group_manager
+
+        # The _src_arr variable holds a numpy record array with the necessary
+        # source information needed for the event selection method.
+        self._src_arr = None
+
+    @property
+    def src_hypo_group_manager(self):
+        """The SourceHypoGroupManager instance, which defines the list of
+        sources.
+        """
+        return self._src_hypo_group_manager
+    @src_hypo_group_manager.setter
+    def src_hypo_group_manager(self, manager):
+        if(not isinstance(manager, SourceHypoGroupManager)):
+            raise TypeError('The src_hypo_group_manager property must be an instance of SourceHypoGroupManager!')
+        self._src_hypo_group_manager = manager
+
+    def change_source_hypo_group_manager(self, src_hypo_group_manager):
+        """Changes the SourceHypoGroupManager instance of the event selection
+        method. This will also recreate the source numpy record array.
+        """
+        self.src_hypo_group_manager = src_hypo_group_manager
+        self._src_arr = self.source_to_array(self._src_hypo_group_manager.source_list)
+
+    @abc.abstractmethod
+    def source_to_array(self, sources):
+        """This method is supposed to convert a sequence of SourceModel
+        instances into a structured numpy ndarray with the source information
+        in a format that is best understood my the actual event selection
+        method.
+
+        Parameters
+        ----------
+        sources : sequence of SourceModel
+            The sequence of source models containing the necessary information
+            of the source.
+
+        Returns
+        -------
+        arr : numpy record ndarray
+            The generated numpy record ndarray holding the necessary information
+            for each source.
+        """
+        pass
 
     @abc.abstractmethod
     def select_events(self, events):
@@ -37,8 +94,21 @@ class EventSelectionMethod(object):
 class AllEventSelectionMethod(EventSelectionMethod):
     """This event selection method selects all events.
     """
-    def __init__(self):
-        super(AllEventSelectionMethod, self).__init__()
+    def __init__(self, src_hypo_group_manager):
+        """Creates a new event selection method instance.
+
+        Parameters
+        ----------
+        src_hypo_group_manager : SourceHypoGroupManager instance
+            The instance of SourceHypoGroupManager that defines the list of
+            sources, i.e. the list of SourceModel instances. For this particular
+            event selection method it has no meaning, but it is an interface
+            parameter.
+        """
+        super(AllEventSelectionMethod, self).__init__(src_hypo_group_manager)
+
+    def source_to_array(self, sources):
+        return None
 
     def select_events(self, events):
         return events
@@ -46,48 +116,26 @@ class AllEventSelectionMethod(EventSelectionMethod):
 
 class SpatialBoxEventSelectionMethod(EventSelectionMethod):
     """This event selection method selects events within a spatial box in
-    right-ascention and declination around a list of sources positions.
+    right-ascention and declination around a list of point-like sources
+    positions.
     """
-    def __init__(self, src_ra, src_dec, delta_angle):
+    def __init__(self, src_hypo_group_manager, delta_angle):
         """Creates and configures a spatial box event selection method object.
 
         Parameters
         ----------
-        src_ra : 1D ndarray
-            The ndarray holding the right-ascention coordinate of all the
-            sources.
-        src_dec : 1D ndarray
-            The ndarray holding the declination coordinate of all the sources.
+        src_hypo_group_manager : SourceHypoGroupManager instance
+            The instance of SourceHypoGroupManager that defines the list of
+            sources, i.e. the list of SourceModel instances.
         delta_angle : float
             The half-opening angle around the source for which events should
             get selected.
         """
-        super(SpatialBoxEventSelectionMethod, self).__init__()
+        super(SpatialBoxEventSelectionMethod, self).__init__(src_hypo_group_manager)
 
-        self.src_ra = src_ra
-        self.src_dec = src_dec
         self.delta_angle = delta_angle
 
-    @property
-    def src_ra(self):
-        """The ndarray holding the right-ascention coordinate of all the
-        sources.
-        """
-        return self._src_ra
-    @src_ra.setter
-    def src_ra(self, arr):
-        arr = np.atleast_1d(arr)
-        self._src_ra = arr
-
-    @property
-    def src_dec(self):
-        """The ndarray holding the declination coordinate of all the sources.
-        """
-        return self._src_dec
-    @src_dec.setter
-    def src_dec(self, arr):
-        arr = np.atleast_1d(arr)
-        self._src_dec = arr
+        self._src_arr = self.source_to_array(self._src_hypo_group_manager.source_list)
 
     @property
     def delta_angle(self):
@@ -99,6 +147,37 @@ class SpatialBoxEventSelectionMethod(EventSelectionMethod):
     def delta_angle(self, angle):
         angle = float_cast(angle, 'The delta_angle property must be castable to type float!')
         self._delta_angle = angle
+
+    def source_to_array(self, sources):
+        """Converts the given sequence of SourceModel instances into a
+        structured numpy ndarray holding the necessary source information needed
+        for this event selection method.
+
+        Parameters
+        ----------
+        sources : sequence of SourceModel
+            The sequence of source models containing the necessary information
+            of the source.
+
+        Returns
+        -------
+        arr : numpy record ndarray
+            The generated numpy record ndarray holding the necessary information
+            for each source. It contains the following data fields: 'ra', 'dec'.
+        """
+        if(not issequenceof(sources, SourceModel)):
+            raise TypeError('The sources argument must be a sequence of SourceModel instances!')
+
+        arr = np.empty(
+            (len(sources),),
+            dtype=[('ra', np.float), ('dec', np.float)],
+            order='F')
+
+        for (i, src) in enumerate(sources):
+            arr['ra'][i] = src.loc.ra
+            arr['dec'][i] = src.loc.dec
+
+        return arr
 
     def select_events(self, events):
         """Selects the events within the spatial box in right-ascention and
@@ -124,8 +203,8 @@ class SpatialBoxEventSelectionMethod(EventSelectionMethod):
 
         # Calculate the minus and plus declination around the source and bound
         # it to -90deg and +90deg, respectively.
-        src_dec_minus = np.maximum(-np.pi/2, self.src_dec - self.delta_angle)
-        src_dec_plus = np.minimum(self.src_dec + self.delta_angle, np.pi/2)
+        src_dec_minus = np.maximum(-np.pi/2, self._src_arr['dec'] - self.delta_angle)
+        src_dec_plus = np.minimum(self._src_arr['dec'] + self.delta_angle, np.pi/2)
 
         # Calculate the cosine factor for the largest declination distance from
         # the source. We use np.amin here because smaller cosine values are
@@ -135,14 +214,14 @@ class SpatialBoxEventSelectionMethod(EventSelectionMethod):
 
         # Calculate delta RA, which is a function of declination.
         # dRA is a (N_sources,)-shaped ndarray.
-        dRA = np.amin([np.repeat(2*np.pi, len(self.src_ra)), 2*self.delta_angle / cosfact], axis=0)
+        dRA = np.amin([np.repeat(2*np.pi, len(self._src_arr['ra'])), 2*self.delta_angle / cosfact], axis=0)
 
         # Calculate the right-ascension distance of the events w.r.t. the
         # source. We make sure to use the smaller distance on the circle, thus
         # the maximal distance is 180deg, i.e. pi.
         # ra_dist is a 2D ndarray with the source on axis 0 and the events on
         # axis 1.
-        ra_dist = np.fabs(np.mod(events['ra'] - self.src_ra[:,np.newaxis] + np.pi, 2*np.pi) - np.pi)
+        ra_dist = np.fabs(np.mod(events['ra'] - self._src_arr['ra'][:,np.newaxis] + np.pi, 2*np.pi) - np.pi)
 
         # Determine the mask for the events which fall inside the
         # right-ascention window.
