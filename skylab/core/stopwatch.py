6e9d3f24587eac6cfa7a62f5a7f93d471bb27ff7
martwo <martin.wolf@tum.de>
2018-10-03 09:48:59 +0000

    Make the output of the Stopwatch more readable and provide a function to call the take_lap method in a consistent way, even when there is no stop watch defined.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@165932 16731396-06f5-0310-8873-f7f720988828

--- skylab/core/stopwatch.py
+++ skylab/core/stopwatch.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import time
+
+from skylab.core import display
+
+def get_sw_lap_taker(sw):
+    """Returns a function with call signature ``__call__(msg)`` to take a lap
+    for the given Stopwatch instance.
+
+    Parameters
+    ----------
+    sw : Stopwatch instance | None
+        The Stopwatch instance for which the lap should be taken. If None, the
+        returned function will do nothing.
+
+    Returns
+    -------
+    func : function with call signature ``__call__(msg)``
+        The function that will take the lap. If ``sw`` was set to None, the
+        returned function will do nothing.
+    """
+    if(sw):
+        sw_take_lap = sw.take_lap
+        return lambda msg: sw_take_lap(msg)
+
+    return lambda msg: False
+
 
 class Stopwatch(object):
     """This class provides a stop-watch functionality to measure time duration
@@ -37,6 +63,7 @@ class Stopwatch(object):
         self._stop_time = None
 
         self._lap_names = []
+        self._max_lap_name_len = 0
 
     def start(self):
         """Starts the stop-watch for the given key.
@@ -72,6 +99,8 @@ class Stopwatch(object):
         """
         if(not self.started):
             raise RuntimeError('Cannot take lap time, because the stop-watch was not started yet!')
+
+        self._max_lap_name_len = np.maximum(self._max_lap_name_len, len(name))
 
         self._lap_names.append(name)
         self._start_times.append(time.time())
@@ -133,7 +162,7 @@ class Stopwatch(object):
         if(not self.stopped):
             s += 'Not stopped yet.'
 
-        max_lap_name = 15
+        max_lap_name = np.minimum(self._max_lap_name_len, display.PAGE_WIDTH-16)
         n_laps = self.n_laps
         for i in range(n_laps):
             lap_name = self._lap_names[i][0:max_lap_name]
