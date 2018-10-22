corrected formula
git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skylab3@166344 16731396-06f5-0310-8873-f7f720988828

skylab/core/signalpdf.py
@@ -290,6 +290,6 @@ class SignalTimePDF(TimePDF, IsSignalPDF):
         # if the time profile is entirly during detector off-time.
         prob = np.zeros((len(events),), dtype=np.float)
         if(self._S > 0):
-            prob[on] = self._time_profile.get_value(events_time[on]) * self._I / self._S
+            prob[on] = self._time_profile.get_value(events_time[on]) / (self._I * self._S)
 
         return prob