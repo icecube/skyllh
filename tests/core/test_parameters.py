284f4eb19a823d671102df1f93bff384c6316f25
martwo <martin.wolf@tum.de>
2018-11-22 15:15:00 +0000

    Fix broken test for MultiSourceFitParameterMapper class.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skyllh@166998 16731396-06f5-0310-8873-f7f720988828

--- tests/core/test_parameters.py
+++ tests/core/test_parameters.py
@@ -9,7 +9,6 @@ def isAlmostEqual(a, b, decimals=9):
     return np.all(np.around(np.abs(a - b), decimals) == 0)
 
 
-@unittest.skip("Skipping old failing test.")
 class TestParameters(unittest.TestCase):
     def test_ParameterGridSet_parameter_permutation_dict_list(self):
         """Test the arameter_permutation_dict_list method of the
@@ -28,7 +27,7 @@ class TestParameters(unittest.TestCase):
         paramgridset += paramgrid
 
         perm_dict_list = paramgridset.parameter_permutation_dict_list
-        self.assertTrue(isAlmostEqual( [ d['gamma'] for d in perm_dict_list ], GAMMA_GRID))
+        self.assertTrue(isAlmostEqual([ d['gamma'] for d in perm_dict_list ], GAMMA_GRID))
 
         paramgrid = make_linear_parameter_grid_1d(name='Ecut', low=9., high=9.1, delta=0.1)
         self.assertTrue(isAlmostEqual(paramgrid.grid, ECUT_GRID))
@@ -36,39 +35,39 @@ class TestParameters(unittest.TestCase):
         paramgridset += paramgrid
 
         perm_dict_list = paramgridset.parameter_permutation_dict_list
-        self.assertTrue(isAlmostEqual( [ d['Ecut'] for d in perm_dict_list ], ECUT_GRID*len(GAMMA_GRID)))
+        self.assertTrue(isAlmostEqual([ d['Ecut'] for d in perm_dict_list ], ECUT_GRID*len(GAMMA_GRID)))
 
-    def test_SourceFitParameterManager(self):
-        from skyllh.physics.flux import PowerLawFlux
+    def test_MultiSourceFitParameterMapper(self):
+        from skyllh.core.random import RandomStateService
         from skyllh.physics.source import PointLikeSource
-        from skyllh.core.parameters import SourceFitParameterManager, FitParameter
+        from skyllh.core.parameters import MultiSourceFitParameterMapper, FitParameter
 
-        # Define the flux model.
-        fluxmodel = PowerLawFlux(1, 1e3, -2)
+        # Define a random state service.
+        rss = RandomStateService(seed=0)
 
         # Define a list of point-like sources.
         sources = [
-            PointLikeSource(np.deg2rad(120), np.deg2rad(-23), fluxmodel),
-            PointLikeSource(np.deg2rad(266), np.deg2rad(61), fluxmodel),
+            PointLikeSource(np.deg2rad(120), np.deg2rad(-23)),
+            PointLikeSource(np.deg2rad(266), np.deg2rad(61)),
         ]
 
         # Define the fit parameters 'gamma1' and 'gamma2' which map to the
         # 'gamma' source parameter of the first and second source, respectively.
-        sfpm = SourceFitParameterManager(sources)
+        sfpm = MultiSourceFitParameterMapper(rss, sources)
         sfpm.def_fit_parameter(FitParameter('gamma1', 1, 4, 2.0), 'gamma', sources[0])
         sfpm.def_fit_parameter(FitParameter('gamma2', 1, 4, 2.1), 'gamma', sources[1])
 
         # Check the initial values.
-        self.assertTrue(np.all(sfpm.initials == np.array([2.0, 2.1])))
+        self.assertTrue(np.all(sfpm.fitparamset.initials == np.array([2.0, 2.1])))
 
         # Get the source parameters for the first source (feed it with the
         # initials).
-        fitparams = sfpm.get_source_fit_params(0, sfpm.initials)
+        fitparams = sfpm.get_src_fitparams(sfpm.fitparamset.initials, 0)
         self.assertEqual(fitparams, {'gamma': 2.0})
 
         # Get the source parameters for the second source (feed it with the
         # initials).
-        fitparams = sfpm.get_source_fit_params(1, sfpm.initials)
+        fitparams = sfpm.get_src_fitparams(sfpm.fitparamset.initials, 1)
         self.assertEqual(fitparams, {'gamma': 2.1})
 
 if(__name__ == '__main__'):
