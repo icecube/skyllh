dcecd32acc9d75112a8bb63f972f19c7c874d24c
martwo <martin.wolf@tum.de>
2019-02-05 17:52:37 +0000

    Added background event generation method to sample from monte-carlo data.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skyllh@171144 16731396-06f5-0310-8873-f7f720988828

--- skyllh/core/background_generation.py
+++ skyllh/core/background_generation.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import numpy as np
+
+from skyllh.core.py import (
+    float_cast,
+    func_has_n_args
+)
+from skyllh.core.dataset import tidy_up_recarray
 
 
 class BackgroundGenerationMethod(object):
@@ -42,3 +49,136 @@ class BackgroundGenerationMethod(object):
             The numpy record arrays holding the generated background events.
         """
         pass
+
+
+class MCDataSamplingBkgGenMethod(BackgroundGenerationMethod):
+    """This class implements the method to generate background events from
+    monte-carlo (MC) data by sampling events from the MC data set according to a
+    probability value given for each event. Functions can be provided to get the
+    mean number of background events and the probability of each monte-carlo
+    event.
+    """
+    def __init__(self, get_event_prob_func, get_mean_func=None):
+        """Creates a new instance of the MCDataSamplingBkgGenMethod class.
+
+        Parameters
+        ----------
+        get_event_prob_func : callable
+            The function to get the background probability of each monte-carlo
+            event. The call signature of this function must be
+                `__call__(dataset, data)`,
+            where `dataset` and `data` are `Dataset` and `DatasetData` instances
+            of the data set for which background events needs to get generated.
+        get_mean_func : callable | None
+            The function to get the mean number of background events.
+            The call signature of this function must be
+                `__call__(dataset, data)`,
+            where `dataset` and `data` are `Dataset` and `DatasetData` instances
+            of the data set for which background events needs to get generated.
+            This argument can be `None`, which means that the mean number of
+            background events to generate needs to get specified through the
+            `generate_events` method.
+        """
+        super(MCDataSamplingBkgGenMethod, self).__init__()
+
+        self.get_event_prob_func = get_event_prob_func
+        self.get_mean_func = get_mean_func
+
+        # Define cache members to cache the background probabilities for each
+        # monte-carlo event. The probabilities change only if the data changes.
+        self._cache_data_id = None
+        self._cache_mc_event_bkg_prob = None
+        self._cache_mean = None
+
+    @property
+    def get_event_prob_func(self):
+        """The function to obtain the background probability for each
+        monte-carlo event of the data set.
+        """
+        return self._get_event_prob_func
+    @get_event_prob_func.setter
+    def get_event_prob_func(self, func):
+        if(not callable(func)):
+            raise TypeError('The get_event_prob_func property must be a '
+                'callable!')
+        if(not func_has_n_args(func, 2)):
+            raise TypeError('The function provided for the get_event_prob_func '
+                'property must have 2 arguments!')
+        self._get_event_prob_func = func
+
+    @property
+    def get_mean_func(self):
+        """The function to obtain the mean number of background events for the
+        data set. This can be None, which means that the mean number of
+        background events to generate needs to be specified through the
+        `generate_events` method.
+        """
+        return self._get_mean_func
+    @get_mean_func.setter
+    def get_mean_func(self, func):
+        if(func is not None):
+            if(not callable(func)):
+                raise TypeError('The get_mean_func property must be a '
+                    'callable!')
+            if(not func_has_n_args(func, 2)):
+                raise TypeError('The function provided for the get_mean_func '
+                    'property must have 2 arguments!')
+        self._get_mean_func = func
+
+    def generate_events(self, rss, dataset, data, mean):
+        """Generates background events  a `mean` number of background
+        events for the given dataset and its data.
+
+        Parameters
+        ----------
+        rss : instance of RandomStateService
+            The instance of RandomStateService that should be used to generate
+            random numbers from.
+        dataset : instance of Dataset
+            The Dataset instance describing the dataset for which background
+            events should get generated.
+        data : instance of DatasetData
+            The DatasetData instance holding the data of the dataset for which
+            background events should get generated.
+        mean : float | None
+            The mean number of background events to generate.
+            Can be `None`. In that case the mean number of background events is
+            obtained through the `get_mean_func` function.
+
+        Returns
+        -------
+        bkg_events : numpy record array
+            The numpy record arrays holding the generated background events.
+        """
+        data_id = id(data)
+        if(self._cache_data_id != data_id):
+            # Dataset has changed. We need to get new background probabilities
+            # for each monte-carlo event and a new mean number of background
+            # events.
+            self._cache_data_id = data_id
+            self._cache_mc_event_bkg_prob = self._get_event_prob_func(dataset, data)
+            if(self._get_mean_func is not None):
+                self._cache_mean = self._get_mean_func(dataset, data)
+
+        if(mean is None):
+            if(self._cache_mean is None):
+                raise ValueError('No mean number of background events and no '
+                    'get_mean_func were specified! One of the two must be '
+                    'specified!')
+            mean = self._cache_mean
+
+        mean = float_cast(mean, 'The mean number of background events must be '
+            'castable to type float!')
+
+        # Draw the number of background events from a poisson distribution with
+        # the given mean number of background events.
+        n_bkg = rss.poisson(mean)
+
+        # Draw the actual background events from the monto-carlo data set.
+        bkg_events = rss.choice(data.mc, size=n_bkg, p=self._cache_mc_event_bkg_prob)
+
+        # Remove MC specific data fields from the background events record
+        # array. So the result contains only experimental data fields.
+        bkg_events = tidy_up_recarray(bkg_events, data.exp_field_names)
+
+        return bkg_events
