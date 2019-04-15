fcdbb1eb1bf11385f0286bc7aaa430c539a173fe
tomaskontrimas <t.kontrimas@outlook.com>
2019-04-15 14:22:17 +0000

    Implemented python logging for multiple processes.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skylab/branches/skyllh@172714 16731396-06f5-0310-8873-f7f720988828

--- skyllh/__init__.py
+++ skyllh/__init__.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from skyllh.core.debugging import setup_logging
 
-# Set logging levels of console and file output logs.
-CONSOLE_LEVEL = logging.INFO
-FILE_LEVEL = logging.DEBUG
-
-# Set log file size and number of backup files.
-max_bytes = 10*1024*1024 # 10 MB
-backup_count = 1
-
-setup_logging(CONSOLE_LEVEL, FILE_LEVEL, max_bytes, backup_count)
+# Initialize top-level logger with a do-nothing NullHandler.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
