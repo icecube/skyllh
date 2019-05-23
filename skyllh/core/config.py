0dd9012d0dce9e644a894a6ee18242a966e5eded
tomaskontrimas <t.kontrimas@outlook.com>
2019-05-23 09:53:37 +0000

    Update global config variable
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skyllh/skyllh/branches/config@173430 16731396-06f5-0310-8873-f7f720988828

--- skyllh/core/config.py
+++ skyllh/core/config.py
@@ -3,20 +3,22 @@
 """This file contains global configuration dictionary.
 """
 
-cfg = {
+CFG = {
     'dirs': {
         # A working directory path to save created files.
         'wd': '.'
     },
-    'mp': {
+    'multiproc': {
         # The number of CPUs to use for functions that allow multi-processing.
         # If this setting is set to an int value in the range [1, N] this
         # setting will be used if a function's local ncpu setting is not
         # specified.
         'ncpu': None
     },
+    'project': {},
     'repository': {
         # A base path of repository datasets.
         'base_path': None
-    }
+    },
+    'user': {}
 }
