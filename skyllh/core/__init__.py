78900ffca0088e95468b23305ec43faae5210d01
martwo <martin.wolf@tum.de>
2019-07-18 13:52:14 +0000

    Add function to generate a working directory file name
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skyllh/skyllh/trunk@174684 16731396-06f5-0310-8873-f7f720988828

--- skyllh/core/__init__.py
+++ skyllh/core/__init__.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+
 from skyllh.core import session
+from skyllh.core.config import CFG
 
 # Automatically enable interactive mode, if the Python interpreter is in
 # interactive mode.
@@ -6,3 +11,23 @@ if(session.is_python_interpreter_in_interactive_mode()):
     session.enable_interactive_session()
 else:
     session.disable_interactive_session()
+
+
+def wd_filename(filename):
+    """Generates the fully qualified file name under the project's working
+    directory of the given file.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file for which to generate the working directory path
+        file name.
+
+    Returns
+    -------
+    pathfilename : str
+        The generated fully qualified path file name of ``filename`` with the
+        project's working directory prefixed.
+    """
+    pathfilename = os.path.join(CFG['project']['working_directory'], filename)
+    return pathfilename
