00578e68672162ef6fb03629e29f2338677c3410
martwo <martin.wolf@tum.de>
2019-10-22 10:06:30 +0000

    Increase the flexibility of the plot_ns_fit_vs_mean_ns_inj plotting function
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skyllh/skyllh/trunk@176306 16731396-06f5-0310-8873-f7f720988828

--- skyllh/plotting/utils/trials.py
+++ skyllh/plotting/utils/trials.py
@@ -12,8 +12,11 @@ def plot_ns_fit_vs_mean_ns_inj(
         title='',
         axis_fontsize=16,
         title_fontsize=16,
+        return_hist=False,
         mean_n_sig_key='mean_n_sig',
-        ns_key='ns'):
+        ns_key='ns',
+        xlabel=None,
+        ylabel=None):
     """Creates a 2D histogram plot showing the fit number of signal events vs.
     the mean number of injected signal events.
 
@@ -27,11 +30,38 @@ def plot_ns_fit_vs_mean_ns_inj(
         The size of the font for axis labels.
     title_fontsize : float
         The size of the font for the title.
+    return_hist : bool
+        If set to ``True``, the histogram data will be return as well.
+        Default is ``False``.
+    mean_n_sig_key : str
+        The name of the key for the mean number of injected signal events in the
+        given trials record array.
+        Default is ``'mean_n_sig'``.
+    ns_key : str
+        The name of the key for the fitted number of signal events in the
+        given trials record array.
+        Default is ``'ns'``.
+    xlabel : str | None
+        The label of the x-axis in math syntax.
+        Default is ``r'<n>_{\mathrm{sig,inj}}}'``.
+    ylabel : str | None
+        The label of if y-axis in math syntax.
+        Default is ``r'n_\mathrm{sig,fit}'``.
+
     Returns
     -------
     fig : MPL Figure instance
         The created matplotlib Figure instance.
+    hist : 2d ndarray
+        If the ``return_hist`` option was set to ``True``, this will be
+        returned as well. It contains the histogram bin content.
+
     """
+    if(xlabel is None):
+        xlabel = r'<n>_{\mathrm{sig,inj}}'
+    if(ylabel is None):
+        ylabel = r'n_\mathrm{sig,fit}'
+
     mean_n_sig_min = np.min(trials[mean_n_sig_key])
     mean_n_sig_max = np.max(trials[mean_n_sig_key])
     mean_n_sig_step = np.diff(np.sort(np.unique(trials[mean_n_sig_key])))[0]
@@ -42,10 +72,12 @@ def plot_ns_fit_vs_mean_ns_inj(
 
     reco_ns_min = np.min(trials[ns_key])
     reco_ns_max = np.max(trials[ns_key])
-    y_bins = np.arange(reco_ns_min, reco_ns_max, mean_n_sig_step)
+    dy = np.min((mean_n_sig_step, np.abs(reco_ns_max-reco_ns_min)/100))
+    y_bins = np.arange(np.floor(reco_ns_min), reco_ns_max+dy, dy)
 
     hist_weights = np.ones_like(trials[mean_n_sig_key])
-    (mean_n_sig, n_trials) = np.unique(trials[mean_n_sig_key], return_counts=True)
+    (mean_n_sig, n_trials) = np.unique(
+        trials[mean_n_sig_key], return_counts=True)
     ns_median = np.empty_like(mean_n_sig, dtype=np.float)
     ns_uq = np.empty_like(mean_n_sig, dtype=np.float)
     ns_lq = np.empty_like(mean_n_sig, dtype=np.float)
@@ -62,15 +94,14 @@ def plot_ns_fit_vs_mean_ns_inj(
     ax_divider = make_axes_locatable(ax[0])
     # Add an axes above the main axes for the colorbar.
     cax = ax_divider.append_axes("top", size="7%", pad="2%")
-    (h, xedges, yedges, image) = ax[0].hist2d(
+    (hist, xedges, yedges, image) = ax[0].hist2d(
         trials[mean_n_sig_key], trials['ns'],
         bins=[x_bins, y_bins],
         weights=hist_weights,
         norm=mpl.colors.LogNorm(),
         cmap=plt.get_cmap('GnBu'))
 
-    ax[0].set_ylabel(
-        '$n_\mathrm{sig,fit}$', fontsize=axis_fontsize)
+    ax[0].set_ylabel('$'+ylabel+'$', fontsize=axis_fontsize)
 
     # Add diagonal line.
     ax[0].plot(
@@ -87,8 +118,7 @@ def plot_ns_fit_vs_mean_ns_inj(
         0.5, 1, title, horizontalalignment='center', verticalalignment='bottom',
         transform=fig.transFigure, fontsize=title_fontsize)
 
-
-    ax[1].hlines(x_bins[0], x_bins[-1], 0)
+    ax[1].hlines(0, x_bins[0], x_bins[-1])
     m = mean_n_sig != 0
     ax[1].plot(
         mean_n_sig[m], (ns_median[m]-mean_n_sig[m])/mean_n_sig[m]*100,
@@ -98,18 +128,18 @@ def plot_ns_fit_vs_mean_ns_inj(
         (ns_uq[m]-mean_n_sig[m])/mean_n_sig[m]*100,
         (ns_lq[m]-mean_n_sig[m])/mean_n_sig[m]*100,
         alpha=0.2, color='gray', label=r'$1\sigma$')
+
     ax[1].legend()
 
-    ax[1].set_xlabel(
-        '$<n>_{\mathrm{sig,inj}}}$',
-        fontsize=axis_fontsize)
-    ax[1].set_ylabel(
-        r'$\frac{n_{\mathrm{sig,fit}} - <n>_{\mathrm{sig,inj}}}'
-        '{<n>_{\mathrm{sig,inj}}}$ [%]',
-        fontsize=axis_fontsize)
+    ax[1].set_xlabel('$'+xlabel+'$', fontsize=axis_fontsize)
+    ratio_ylabel = r'$\frac{%s - %s}{%s}$'%(ylabel, xlabel, xlabel)+' [%]'
+    ax[1].set_ylabel(ratio_ylabel, fontsize=axis_fontsize)
     ax[1].set_xlim(x_bins[0], x_bins[-1])
     ax[1].set_ylim([-100, 100])
 
     plt.tight_layout()
+
+    if(return_hist):
+        return (fig, hist, xedges, yedges)
 
     return fig
