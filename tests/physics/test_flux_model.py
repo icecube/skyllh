d5f6f1991087145509c38e40006523f55fee5bf7
martwo <martin.wolf@tum.de>
2020-02-24 17:36:25 +0000

    Update flux model test
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/skyllh/skyllh/trunk@179063 16731396-06f5-0310-8873-f7f720988828

--- tests/physics/test_flux_model.py
+++ tests/physics/test_flux_model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from astropy import units
 
-from skyllh.physics.flux_models import (
+from skyllh.physics.flux_model import (
     UnitySpatialFluxProfile,
     PointSpatialFluxProfile,
     EnergyFluxProfile,
@@ -24,66 +24,83 @@ from skyllh.physics.flux_models import (
 from skyllh.core.py import float_cast
 
 
-class TestUnitySpatialFluxProfile(unittest.TestCase):
+class UnitySpatialFluxProfileTestCase(unittest.TestCase):
     def setUp(self):
-        self.instance = UnitySpatialFluxProfile()
+        self.fluxprofile = UnitySpatialFluxProfile()
 
     def test_math_function_str(self):
-        self.assertEqual(self.instance.math_function_str(), '1')
+        try:
+            math_func_str = self.fluxprofile.math_function_str
+        except:
+            self.fail('The math_function_str property raised an exception!')
 
     def test_call(self):
         alpha = 1.5
         delta = 2.5
 
-        np.testing.assert_array_equal(self.instance(alpha, delta),
+        np.testing.assert_array_equal(self.fluxprofile(alpha, delta),
                                       np.array([1]))
 
         alpha = np.array([1.5, 2])
         delta = np.array([2.5, 3])
 
-        np.testing.assert_array_equal(self.instance(alpha, delta),
+        np.testing.assert_array_equal(self.fluxprofile(alpha, delta),
                                       np.array([1, 1]))
 
-class TestPointSpatialFluxProfile(unittest.TestCase):
+class PointSpatialFluxProfileTestCase(unittest.TestCase):
     def setUp(self):
         self.alpha_s = 1.5
         self.delta_s = 2.5
-        self.instance = PointSpatialFluxProfile(self.alpha_s, self.delta_s)
+        self.fluxprofile = PointSpatialFluxProfile(
+            self.alpha_s, self.delta_s, angle_unit=units.radian)
 
     def test_init(self):
-        self.assertEqual(self.instance.alpha_s, self.alpha_s)
-        self.assertEqual(self.instance.delta_s, self.delta_s)
+        self.assertEqual(self.fluxprofile.alpha_s, self.alpha_s)
+        self.assertEqual(self.fluxprofile.delta_s, self.delta_s)
+
+    def test_param_names(self):
+        param_names = self.fluxprofile.param_names
+        print(param_names)
+        self.assertEqual(len(param_names), 2)
+        self.assertEqual(param_names[0], 'alpha_s')
+        self.assertEqual(param_names[1], 'delta_s')
+
+    def test_angle_unit(self):
+        self.assertEqual(self.fluxprofile.angle_unit, units.radian)
 
     def test_alpha_s(self):
-        alpha_s_new = 5.5
-        delta_s_new = 6.5
-        self.instance.alpha_s = alpha_s_new
-        self.instance.delta_s_new = delta_s_new
+        alpha_s = 5.5
+        self.fluxprofile.alpha_s = alpha_s
+        self.assertAlmostEqual(self.fluxprofile.alpha_s, alpha_s)
 
-        self.assertEqual(self.instance.alpha_s, alpha_s_new)
-        self.assertEqual(self.instance.delta_s, delta_s_new)
+    def test_delta_s(self):
+        delta_s = -1.4
+        self.fluxprofile.delta_s = delta_s
+        self.assertAlmostEqual(self.fluxprofile.delta_s, delta_s)
 
     def test_math_function_str(self):
-        self.assertEqual(self.instance.math_function_str(),
-                         'delta(alpha-%.2e)*delta(delta-%.2e)'%(self._alpha_s, self._delta_s))
+        try:
+            math_func_str = self.fluxprofile.math_function_str
+        except:
+            self.fail('The math_function_str property raised an exception!')
 
     def test_call(self):
         alpha = 1.5
         delta = 2.5
 
-        np.testing.assert_array_equal(self.instance(alpha, delta),
+        np.testing.assert_array_equal(self.fluxprofile(alpha, delta),
                                       np.array([1]))
 
         alpha = np.array([1.5, 2])
         delta = np.array([2.5, 3])
 
-        np.testing.assert_array_equal(self.instance(alpha, delta),
+        np.testing.assert_array_equal(self.fluxprofile(alpha, delta),
                                       np.array([1, 0]))
 
 @unittest.skip("Skip")
 class TestFactorizedFluxModel(unittest.TestCase):
     def setUp(self):
-        self.flux_model = FluxModel()
+        self.flux_model = FactorizedFluxModel()
 
     def test_init(self):
         self.assertEqual(self.flux_model.angle_unit, units.radian)
