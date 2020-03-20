118063d4d65c7441820857e834d997d51fb2a1de
Hans Niederhausen <hans.niederhausen@protonmail.com>
2020-03-20 13:07:40 +0100

    do not perform photospline fits. instead load from splinetable on disk.

--- skyllh/core/utils/multidimgridpdf.py
+++ skyllh/core/utils/multidimgridpdf.py
@@ -5,6 +5,7 @@ MultiDimGridPDF instances.
 """
 
 import numpy as np
+import os
 
 from skyllh.core.binning import BinningDefinition
 from skyllh.core.pdf import MultiDimGridPDF
@@ -29,6 +30,81 @@ def kde_pdf_bkg_norm_factor_func(pdf, tdm, fitparams):
     ``create_MultiDimGridPDF_from_kde_pdf`` function.
     """
     return 1. / (2 * np.pi)
+
+def create_MultiDimGridPDF_from_photosplinetable(
+        ds, data, info_key, splinetable_key, norm_factor_func=None,
+        kind=None, tl=None):
+    """Creates a MultiDimGridPDF instance with pdf values taken from KDE PDF
+    values stored in the dataset's auxiliary data.
+
+    Parameters
+    ----------
+    ds : Dataset instance
+        The Dataset instance the PDF applies to.
+    data : DatasetData instance
+        The DatasetData instance that holds the auxiliary data of the data set.
+    info_key : str
+        The auxiliary data name for the PDF information (e.g. axis definitions)
+    splinetable_key : str 
+        The auxiliary data name for the name of the splinetablefile 
+    norm_factor_func : callable | None
+        The normalization factor function. It must have the following call
+        signature:
+            __call__(pdf, tdm, fitparams)
+    kind : str | None
+        The kind of PDF to create. This is either ``'sig'`` for a
+        SignalMultiDimGridPDF or ``'bkg'`` for a BackgroundMultiDimGridPDF
+        instance. If set to None, a MultiDimGridPDF instance is created.
+    tl : TimeLord instance | None
+        The optional TimeLord instance to use for measuring timing information.
+
+    Returns
+    -------
+    pdf : SignalMultiDimGridPDF instance | BackgroundMultiDimGridPDF instance |
+          MultiDimGridPDF instance
+        The created PDF instance. Depending on the ``kind`` argument, this is
+        a SignalMultiDimGridPDF, a BackgroundMultiDimGridPDF, or a
+        MultiDimGridPDF instance.
+    """
+
+
+    if(kind is None):
+        pdf_type = MultiDimGridPDF
+    elif(kind == 'sig'):
+        pdf_type = SignalMultiDimGridPDF
+    elif(kind == 'bkg'):
+        pdf_type = BackgroundMultiDimGridPDF
+    else:
+        raise ValueError('The kind argument must be None, "sig", or "bkg"! '
+            'Currently it is '+str(kind)+'!')
+
+    # Load the PDF data from the auxilary files.
+    num_dict = ds.load_aux_data(info_key, tl=tl)  
+
+    kde_pdf_axis_name_map = ds.load_aux_data('KDE_PDF_axis_name_map', tl=tl)
+    kde_pdf_axis_name_map_inv = dict(
+        [(v,k) for (k,v) in kde_pdf_axis_name_map.items()])
+
+    axis_binnings = [
+        BinningDefinition(
+            kde_pdf_axis_name_map_inv[var], num_dict['bins'][idx])
+        for (idx,var) in enumerate(num_dict['vars'])
+    ]
+ 
+    # get splinetable file
+    splinetable_files = ds.get_aux_data_definition(splinetable_key)
+    assert(len(splinetable_files)==1)
+    splinetable_file = os.path.join(ds.root_dir, splinetable_files[0])
+
+    pdf = pdf_type(
+        axis_binnings,
+        pdf_path_to_splinetable=splinetable_file,
+        pdf_grid_data=None,
+        norm_factor_func=norm_factor_func)
+
+    return pdf
+
+
 
 def create_MultiDimGridPDF_from_kde_pdf(
         ds, data, numerator_key, denumerator_key=None, norm_factor_func=None,
@@ -124,7 +200,8 @@ def create_MultiDimGridPDF_from_kde_pdf(
 
     pdf = pdf_type(
         axis_binnings,
-        vals,
-        norm_factor_func)
+        pdf_path_to_splinetable=None,
+        pdf_grid_data=vals,
+        norm_factor_func=norm_factor_func)
 
     return pdf
