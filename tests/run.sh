47718bde58b4fe102d5eea8255e67c39a599ad2e
Martin Wolf <martin.wolf@tum.de>
2020-03-27 12:10:58 +0100

    Fix tests/run.sh to be executable on the build bots

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-/usr/bin/env python -m unittest discover ./core
-/usr/bin/env python -m unittest discover ./i3
-/usr/bin/env python -m unittest discover ./physics
+# This script must be executed from the skyllh main directory.
+/usr/bin/env python -m unittest discover tests/core
+/usr/bin/env python -m unittest discover tests/i3
+/usr/bin/env python -m unittest discover tests/physics
