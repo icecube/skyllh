549802c09106b7025b1d917e185d7616f34af47e
Martin Wolf, PhD <martwo@users.noreply.github.com>
2022-06-01 13:59:56 +0200

    Update skyllh/core/pdf.py
    Co-authored-by: Tomas Kontrimas <52071038+tomaskontrimas@users.noreply.github.com>

--- skyllh/core/pdf.py
+++ skyllh/core/pdf.py
@@ -1614,7 +1614,7 @@ class MultiDimGridPDFSet(PDF, PDFSet):
         if(isinstance(self, IsSignalPDF)):
             # Evaluate the relevant quantities for
             # all events and sources (relevant for stacking analyses).
-            if(tdm.src_ev_idxs is not None):
+            if tdm.src_ev_idxs is not None:
                 (src_idxs, ev_idxs) = tdm.src_ev_idxs
                 eventdata = np.array(
                     [
