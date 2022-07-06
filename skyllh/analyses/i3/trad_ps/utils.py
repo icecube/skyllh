82fc3c0c6e82a8a3102b04cb0ebe8572afeabc8c
Martin Wolf <mail@martin-wolf.org>
2022-07-06 17:33:39 +0200

    commit current state of the art

--- skyllh/analyses/i3/trad_ps/utils.py
+++ skyllh/analyses/i3/trad_ps/utils.py
@@ -12,90 +12,81 @@ from skyllh.core.binning import (
 from skyllh.core.storage import create_FileLoader
 
 
-def load_effective_area_array(pathfilenames):
-    """Loads the (nbins_sin_true_dec, nbins_log_true_e)-shaped 2D effective
-    area array from the given data file.
+class FctSpline2D(object):
+    """Class to represent a 2D function spline using the RectBivariateSpline
+    class from scipy.
 
-    Parameters
-    ----------
-    pathfilename : str | list of str
-        The file name of the data file.
+    The spline is constructed in the log10 space of the function value to
+    ensure a smooth spline.
 
-    Returns
-    -------
-    arr : (nbins_sin_true_dec, nbins_log_true_e)-shaped 2D ndarray
-        The ndarray holding the effective area for each
-        sin(dec_true),log(e_true) bin.
-    sin_true_dec_binedges_lower : (nbins_sin_true_dec,)-shaped ndarray
-        The ndarray holding the lower bin edges of the sin(dec_true) axis.
-    sin_true_dec_binedges_upper : (nbins_sin_true_dec,)-shaped ndarray
-        The ndarray holding the upper bin edges of the sin(dec_true) axis.
-    log_true_e_binedges_lower : (nbins_log_true_e,)-shaped ndarray
-        The ndarray holding the lower bin edges of the log(E_true) axis.
-    log_true_e_binedges_upper : (nbins_log_true_e,)-shaped ndarray
-        The ndarray holding the upper bin edges of the log(E_true) axis.
+    The evaluate the spline, use the ``__call__`` method.
     """
-    loader = create_FileLoader(pathfilenames=pathfilenames)
-    data = loader.load_data()
-    renaming_dict = {
-        'log10(E_nu/GeV)_min': 'log_true_e_min',
-        'log10(E_nu/GeV)_max': 'log_true_e_max',
-        'Dec_nu_min[deg]':     'sin_true_dec_min',
-        'Dec_nu_max[deg]':     'sin_true_dec_max',
-        'A_Eff[cm^2]':         'a_eff'
-    }
-    data.rename_fields(renaming_dict, must_exist=True)
+    def __init__(self, f, x_binedges, y_binedges, **kwargs):
+        """Creates a new 2D function spline.
 
-    # Convert the true neutrino declination from degrees to radians and into
-    # sin values.
-    data['sin_true_dec_min'] = np.sin(np.deg2rad(
-        data['sin_true_dec_min']))
-    data['sin_true_dec_max'] = np.sin(np.deg2rad(
-        data['sin_true_dec_max']))
+        Parameters
+        ----------
+        f : (n_x, n_y)-shaped 2D numpy ndarray
+            he numpy ndarray holding the function values at the bin centers.
+        x_binedges : (n_x+1,)-shaped 1D numpy ndarray
+            The numpy ndarray holding the bin edges of the x-axis.
+        y_binedges : (n_y+1,)-shaped 1D numpy ndarray
+            The numpy ndarray holding the bin edges of the y-axis.
+        """
+        super().__init__(**kwargs)
 
-    # Determine the binning for energy and declination.
-    log_true_e_binedges_lower = np.unique(
-        data['log_true_e_min'])
-    log_true_e_binedges_upper = np.unique(
-        data['log_true_e_max'])
-    sin_true_dec_binedges_lower = np.unique(
-        data['sin_true_dec_min'])
-    sin_true_dec_binedges_upper = np.unique(
-        data['sin_true_dec_max'])
+        self.x_binedges = np.copy(x_binedges)
+        self.y_binedges = np.copy(y_binedges)
 
-    if(len(log_true_e_binedges_lower) != len(log_true_e_binedges_upper)):
-        raise ValueError('Cannot extract the log10(E/GeV) binning of the '
-            'effective area from data file "{}". The number of lower and upper '
-            'bin edges is not equal!'.format(str(loader.pathfilename_list)))
-    if(len(sin_true_dec_binedges_lower) != len(sin_true_dec_binedges_upper)):
-        raise ValueError('Cannot extract the Dec_nu binning of the effective '
-            'area from data file "{}". The number of lower and upper bin edges '
-            'is not equal!'.format(str(loader.pathfilename_list)))
+        self.x_min = self.x_binedges[0]
+        self.x_max = self.x_binedges[-1]
+        self.y_min = self.y_binedges[0]
+        self.y_max = self.y_binedges[-1]
 
-    nbins_log_true_e = len(log_true_e_binedges_lower)
-    nbins_sin_true_dec = len(sin_true_dec_binedges_lower)
+        x = get_bincenters_from_binedges(x_binedges)
+        y = get_bincenters_from_binedges(y_binedges)
 
-    # Construct the 2d array for the effective area.
-    arr = np.zeros((nbins_sin_true_dec, nbins_log_true_e), dtype=np.double)
+        # Note: For simplicity we approximate zero bins with 1000x smaller
+        # values than the minimum value. To do this correctly, one should store
+        # the zero bins and return zero when those bins are requested.
+        z = np.empty(f.shape, dtype=np.double)
+        m = f > 0
+        z[m] = np.log10(f[m])
+        z[np.invert(m)] = np.min(z[m]) - 3
 
-    sin_true_dec_idx = np.digitize(
-        0.5*(data['sin_true_dec_min'] +
-             data['sin_true_dec_max']),
-            sin_true_dec_binedges_lower) - 1
-    log_true_e_idx = np.digitize(
-        0.5*(data['log_true_e_min'] +
-             data['log_true_e_max']),
-        log_true_e_binedges_lower) - 1
+        self.spl_log10_f = interpolate.RectBivariateSpline(
+            x, y, z, kx=3, ky=3, s=0)
 
-    arr[sin_true_dec_idx, log_true_e_idx] = data['a_eff']
+    def __call__(self, x, y, oor_value=0):
+        """Evaluates the spline at the given coordinates. For coordinates
+        outside the spline's range, the oor_value is returned.
 
-    return (
-        arr,
-        sin_true_dec_binedges_lower,
-        sin_true_dec_binedges_upper,
-        log_true_e_binedges_lower,
-        log_true_e_binedges_upper
-    )
+        Parameters
+        ----------
+        x : (n_x,)-shaped 1D numpy ndarray
+            The numpy ndarray holding the x values at which the spline should
+            get evaluated.
+        y : (n_y,)-shaped 1D numpy ndarray
+            The numpy ndarray holding the y values at which the spline should
+            get evaluated.
+        oor_value : float
+            The value for out-of-range (oor) coordinates.
+
+        Returns
+        -------
+        f : (n_x, n_y)-shaped 2D numpy ndarray
+            The numpy ndarray holding the evaluated values of the spline.
+        """
+        m_x_oor = (x < self.x_min) | (x > self.x_max)
+        m_y_oor = (y < self.y_min) | (y > self.y_max)
+
+        (m_xx_oor, m_yy_oor) = np.meshgrid(m_x_oor, m_y_oor, indexing='ij')
+        m_xy_oor = m_xx_oor | m_yy_oor
+
+        f = np.power(10, self.spl_log10_f(x, y))
+        f[m_xy_oor] = oor_value
+
+        return f
 
 
 def load_smearing_histogram(pathfilenames):
@@ -167,7 +158,7 @@ def load_smearing_histogram(pathfilenames):
         """
         n = 0
         # Select only valid rows.
-        mask = upper_edges - lower_edges > 0
+        mask = (upper_edges - lower_edges) > 0
         data = lower_edges[mask]
         # Go through the valid rows and search for the number of increasing
         # bin edge values.
@@ -529,250 +520,6 @@ def merge_reco_energy_bins(arr, log10_reco_e_binedges, bw_th, max_bw=0.2):
     return (arr, log10_reco_e_binedges)
 
 
-class PublicDataAeff(object):
-    """This class is a helper class for dealing with the effective area
-    provided by the public data.
-    """
-    def __init__(
-            self, pathfilenames, **kwargs):
-        """Creates an effective area instance by loading the effective area
-        data from the given file.
-        """
-        super().__init__(**kwargs)
-
-        (
-            self.aeff_arr,
-            self.sin_true_dec_binedges_lower,
-            self.sin_true_dec_binedges_upper,
-            self.log_true_e_binedges_lower,
-            self.log_true_e_binedges_upper
-        ) = load_effective_area_array(pathfilenames)
-
-        self.sin_true_dec_binedges = np.concatenate(
-            (self.sin_true_dec_binedges_lower,
-             self.sin_true_dec_binedges_upper[-1:])
-        )
-        self.log_true_e_binedges = np.concatenate(
-            (self.log_true_e_binedges_lower,
-             self.log_true_e_binedges_upper[-1:])
-        )
-
-    @property
-    def log_true_e_bincenters(self):
-        """The bin center values of the log true energy axis.
-        """
-        bincenters = 0.5 * (
-            self.log_true_e_binedges[:-1] + self.log_true_e_binedges[1:]
-        )
-
-        return bincenters
-
-    def get_aeff_for_sin_true_dec(self, sin_true_dec):
-        """Retrieves the effective area as function of log_true_e.
-
-        Parameters
-        ----------
-        sin_true_dec : float
-            The sin of the true declination.
-
-        Returns
-        -------
-        aeff : (n,)-shaped numpy ndarray
-            The effective area for the given true declination as a function of
-            log true energy.
-        """
-        sin_true_dec_idx = np.digitize(
-            sin_true_dec, self.sin_true_dec_binedges) - 1
-
-        aeff = self.aeff_arr[sin_true_dec_idx]
-
-        return aeff
-
-    def get_detection_pd_for_sin_true_dec(self, sin_true_dec, true_e):
-        """Calculates the detection probability density p(E_nu|sin_dec) in
-        unit GeV^-1 for the given true energy values.
-
-        Parameters
-        ----------
-        sin_true_dec : float
-            The sin of the true declination.
-        true_e : (n,)-shaped 1d numpy ndarray of float
-            The values of the true energy in GeV for which the probability
-            density value should get calculated.
-
-        Returns
-        -------
-        det_pd : (n,)-shaped 1d numpy ndarray of float
-            The detection probability density values for the given true energy
-            value.
-        """
-        aeff = self.get_aeff_for_sin_true_dec(sin_true_dec)
-
-        dE = np.diff(np.power(10, self.log_true_e_binedges))
-
-        det_pdf = aeff / np.sum(aeff) / dE
-
-        x = np.power(10, self.log_true_e_bincenters)
-        y = det_pdf
-        tck = interpolate.splrep(x, y, k=1, s=0)
-
-        det_pd = interpolate.splev(true_e, tck, der=0)
-
-        return det_pd
-
-    def get_detection_pd_in_log10E_for_sin_true_dec(
-            self, sin_true_dec, log10_true_e):
-        """Calculates the detection probability density p(E_nu|sin_dec) in
-        unit log10(GeV)^-1 for the given true energy values.
-
-        Parameters
-        ----------
-        sin_true_dec : float
-            The sin of the true declination.
-        log10_true_e : (n,)-shaped 1d numpy ndarray of float
-            The log10 values of the true energy in GeV for which the
-            probability density value should get calculated.
-
-        Returns
-        -------
-        det_pd : (n,)-shaped 1d numpy ndarray of float
-            The detection probability density values for the given true energy
-            value.
-        """
-        aeff = self.get_aeff_for_sin_true_dec(sin_true_dec)
-
-        dlog10E = np.diff(self.log_true_e_binedges)
-
-        det_pdf = aeff / np.sum(aeff) / dlog10E
-
-        spl = interpolate.splrep(
-            self.log_true_e_bincenters, det_pdf, k=1, s=0)
-
-        det_pd = interpolate.splev(log10_true_e, spl, der=0)
-
-        return det_pd
-
-    def get_detection_prob_for_sin_true_dec(
-            self, sin_true_dec, true_e_min, true_e_max,
-            true_e_range_min, true_e_range_max):
-        """Calculates the detection probability for a given energy range for a
-        given sin declination.
-
-        Parameters
-        ----------
-        sin_true_dec : float
-            The sin of the true declination.
-        true_e_min : float
-            The minimum energy in GeV.
-        true_e_max : float
-            The maximum energy in GeV.
-        true_e_range_min : float
-            The minimum energy in GeV of the entire energy range.
-        true_e_range_max : float
-            The maximum energy in GeV of the entire energy range.
-
-        Returns
-        -------
-        det_prob : float
-            The true energy detection probability.
-        """
-        true_e_binedges = np.power(10, self.log_true_e_binedges)
-
-        # Get the bin indices for the lower and upper energy range values.
-        (lidx, uidx) = get_bin_indices_from_lower_and_upper_binedges(
-            true_e_binedges[:-1],
-            true_e_binedges[1:],
-            np.array([true_e_range_min, true_e_range_max]))
-        # The function determined the bin indices based on the
-        # lower bin edges. So the bin index of the upper energy range value
-        # is 1 to large.
-        uidx -= 1
-
-        aeff = self.get_aeff_for_sin_true_dec(sin_true_dec)
-        aeff = aeff[lidx:uidx+1]
-        true_e_binedges = true_e_binedges[lidx:uidx+2]
-
-        dE = np.diff(true_e_binedges)
-
-        det_pdf = aeff / dE
-
-        true_e_bincenters = 0.5*(true_e_binedges[:-1] + true_e_binedges[1:])
-        tck = interpolate.splrep(
-            true_e_bincenters, det_pdf,
-            xb=true_e_range_min, xe=true_e_range_max, k=1, s=0)
-
-        def _eval_func(x):
-            return interpolate.splev(x, tck, der=0)
-
-        norm = integrate.quad(
-            _eval_func, true_e_range_min, true_e_range_max,
-            limit=200, full_output=1)[0]
-
-        integral = integrate.quad(
-            _eval_func, true_e_min, true_e_max,
-            limit=200, full_output=1)[0]
-
-        det_prob = integral / norm
-
-        return det_prob
-
-    def get_aeff_integral_for_sin_true_dec(
-            self, sin_true_dec, log_true_e_min, log_true_e_max):
-        """Calculates the integral of the effective area using the trapezoid
-        method.
-
-        Returns
-        -------
-        integral : float
-            The integral in unit cm^2 GeV.
-        """
-        aeff = self.get_aeff_for_sin_true_dec(sin_true_dec)
-
-        integral = (
-            (np.power(10, log_true_e_max) -
-             np.power(10, log_true_e_min)) *
-            0.5 *
-            (np.interp(log_true_e_min, self.log_true_e_bincenters, aeff) +
-             np.interp(log_true_e_max, self.log_true_e_bincenters, aeff))
-        )
-
-        return integral
-
-    def get_aeff(self, sin_true_dec, log_true_e):
-        """Retrieves the effective area for the given sin(dec_true) and
-        log(E_true) value pairs.
-
-        Parameters
-        ----------
-        sin_true_dec : (n,)-shaped 1D ndarray
-            The sin(dec_true) values.
-        log_true_e : (n,)-shaped 1D ndarray
-            The log(E_true) values.
-
-        Returns
-        -------
-        aeff : (n,)-shaped 1D ndarray
-            The 1D ndarray holding the effective area values for each value
-            pair. For value pairs outside the effective area data zero is
-            returned.
-        """
-        valid = (
-            (sin_true_dec >= self.sin_true_dec_binedges[0]) &
-            (sin_true_dec <= self.sin_true_dec_binedges[-1]) &
-            (log_true_e >= self.log_true_e_binedges[0]) &
-            (log_true_e <= self.log_true_e_binedges[-1])
-        )
-        sin_true_dec_idxs = np.digitize(
-            sin_true_dec[valid], self.sin_true_dec_binedges) - 1
-        log_true_e_idxs = np.digitize(
-            log_true_e[valid], self.log_true_e_binedges) - 1
-
-        aeff = np.zeros((len(valid),), dtype=np.double)
-        aeff[valid] = self.aeff_arr[sin_true_dec_idxs,log_true_e_idxs]
-
-        return aeff
-
-
 class PublicDataSmearingMatrix(object):
     """This class is a helper class for dealing with the smearing matrix
     provided by the public data.
@@ -795,6 +542,20 @@ class PublicDataSmearingMatrix(object):
             self.ang_err_lower_edges,
             self.ang_err_upper_edges
         ) = load_smearing_histogram(pathfilenames)
+
+        # Create bin edges array for log10_reco_e.
+        s = np.array(self.reco_e_lower_edges.shape)
+        s[-1] += 1
+        self.log10_reco_e_binedges = np.empty(s, dtype=np.double)
+        self.log10_reco_e_binedges[:,:,:-1] = self.reco_e_lower_edges
+        self.log10_reco_e_binedges[:,:,-1] = self.reco_e_upper_edges[:,:,-1]
+
+        # Create bin edges array for psi.
+        s = np.array(self.psi_lower_edges.shape)
+        s[-1] += 1
+        self.psi_binedges = np.empty(s, dtype=np.double)
+        self.psi_binedges[:,:,:,:-1] = self.psi_lower_edges
+        self.psi_binedges[:,:,:,-1] = self.psi_upper_edges[:,:,:,-1]
 
     @property
     def n_log10_true_e_bins(self):
@@ -837,6 +598,38 @@ class PublicDataSmearingMatrix(object):
         """
         return 0.5*(self._true_dec_bin_edges[:-1] +
                     self._true_dec_bin_edges[1:])
+
+    @property
+    def min_log10_reco_e(self):
+        """(read-only) The minimum value of the reconstructed energy axis.
+        """
+        # Select only valid reco energy bins with bin widths greater than zero.
+        m = (self.reco_e_upper_edges - self.reco_e_lower_edges) > 0
+        return np.min(self.reco_e_lower_edges[m])
+
+    @property
+    def max_log10_reco_e(self):
+        """(read-only) The maximum value of the reconstructed energy axis.
+        """
+        # Select only valid reco energy bins with bin widths greater than zero.
+        m = (self.reco_e_upper_edges - self.reco_e_lower_edges) > 0
+        return np.max(self.reco_e_upper_edges[m])
+
+    @property
+    def min_log10_psi(self):
+        """(read-only) The minimum log10 value of the psi axis.
+        """
+        # Select only valid psi bins with bin widths greater than zero.
+        m = (self.psi_upper_edges - self.psi_lower_edges) > 0
+        return np.min(np.log10(self.psi_lower_edges[m]))
+
+    @property
+    def max_log10_psi(self):
+        """(read-only) The maximum log10 value of the psi axis.
+        """
+        # Select only valid psi bins with bin widths greater than zero.
+        m = (self.psi_upper_edges - self.psi_lower_edges) > 0
+        return np.max(np.log10(self.psi_upper_edges[m]))
 
     @property
     def pdf(self):
