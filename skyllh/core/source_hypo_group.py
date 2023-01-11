a8597dc29f93b2012068385e6d2dce9d78f459fa
Martin Wolf <mail@martin-wolf.org>
2023-01-11 14:49:19 +0100

    Remove obsolete files

--- skyllh/core/source_hypo_group.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-This module is DEPRECATED! Use `skyllh.core.source_hypo_grouping` instead!
-"""
-
-from skyllh.core.source_hypo_grouping import (
-    SourceHypoGroup,
-)
