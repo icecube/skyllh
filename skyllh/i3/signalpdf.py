0d00ce6c03940a90d9d5db880b91a252a7616161
Martin Wolf <mail@martin-wolf.org>
2023-01-12 18:55:28 +0100

    Adapt to new PDF interface

--- skyllh/i3/signalpdf.py
+++ skyllh/i3/signalpdf.py
@@ -41,14 +41,14 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
             The array holding the monte-carlo data. The following data fields
             must exist:
 
-            - 'true_energy' : float
+            true_energy : float
                 The true energy value of the data event.
-            - 'log_energy' : float
-                The logarithm of the reconstructed energy value of the data
-                event.
-            - 'dec' : float
+            log_energy : float
+                The base10-logarithm of the reconstructed energy value of the
+                data event.
+            sin_dec : float
                 The declination of the data event.
-            - 'mcweight' : float
+            mcweight : float
                 The monte-carlo weight value of the data events in unit
                 GeV cm^2 sr.
 
@@ -140,7 +140,7 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
             """
             # Create a copy of the FluxModel with the given flux parameters.
             # The copy is needed to not interfer with other CPU processes.
-            myfluxmodel = fluxmodel.copy(newprop=gridfitparams)
+            myfluxmodel = fluxmodel.copy(newparams=gridfitparams)
 
             # Calculate the signal energy weight of the event. Note, that
             # because we create a normalized PDF, we can ignore all constants.
@@ -149,55 +149,69 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
             data_physicsweight = myfluxmodel(data_true_energy)
 
             i3energypdf = I3EnergyPDF(
-                data_logE, data_sinDec, data_mcweight, data_physicsweight,
-                logE_binning, sinDec_binning, smoothing_filter)
+                pmm=None,
+                data_log_energy=data_logE,
+                data_sin_dec=data_sinDec,
+                data_mcweight=data_mcweight,
+                data_physicsweight=data_physicsweight,
+                log_energy_binning=logE_binning,
+                sin_dec_binning=sinDec_binning,
+                smoothing_filter=smoothing_filter)
 
             return i3energypdf
 
         data_logE = data_mc['log_energy']
-        data_sinDec = np.sin(data_mc['dec'])
+        data_sinDec = data_mc['sin_dec']
         data_mcweight = data_mc['mcweight']
         data_true_energy = data_mc['true_energy']
 
-        args_list = [ ((data_logE, data_sinDec, data_mcweight, data_true_energy,
-                        logE_binning, sinDec_binning, smoothing_filter,
-                        fluxmodel, gridfitparams), {})
-                     for gridfitparams in self.gridfitparams_list ]
+        args_list = [
+            (
+                (data_logE, data_sinDec, data_mcweight, data_true_energy,
+                 logE_binning, sinDec_binning, smoothing_filter,
+                 fluxmodel, gridfitparams),
+                {}
+            )
+            for gridfitparams in self.gridfitparams_list
+        ]
 
         i3energypdf_list = parallelize(
             create_I3EnergyPDF, args_list, self.ncpu, ppbar=ppbar)
 
         # Save all the I3EnergyPDF objects in the IsSignalPDF PDF registry with
         # the hash of the individual parameters as key.
-        for (gridfitparams, i3energypdf) in zip(self.gridfitparams_list, i3energypdf_list):
+        for (gridfitparams, i3energypdf) in zip(self.gridfitparams_list,
+                                                i3energypdf_list):
             self.add_pdf(i3energypdf, gridfitparams)
 
-    def assert_is_valid_for_exp_data(self, data_exp):
-        """Checks if this signal energy PDF is valid for all the given
-        experimental data.
-        It checks if all the data is within the logE and sin(dec) binning range.
+    def assert_is_valid_for_trial_data(self, tdm):
+        """Checks if this signal energy PDF is valid for all the given trial
+        data.
+        It checks if all the data is within the log10(E) and sin(dec) binning
+        range.
 
         Parameters
         ----------
-        data_exp : numpy record ndarray
-            The array holding the experimental data. The following data fields
-            must exist:
+        tdm : instance of TrialDataManager
+            The instance of TrialDataManager holding the trial data events.
+            The following data fields must exist:
 
-            - 'log_energy' : float
-                The logarithm of the energy value of the data event.
-            - 'dec' : float
-                The declination of the data event.
+            log_energy : float
+                The base-10 logarithm of the energy value of the data event.
+            sin_dec : float
+                The sin(declination) of the data event.
 
         Raises
         ------
         ValueError
-            If some of the data is outside the logE or sin(dec) binning range.
+            If some of the data is outside the log10(E) or sin(dec) binning
+            range.
         """
         # Since we use the same binning for all the I3EnergyPDF objects, we
         # can just use an arbitrary object to verify the data.
-        self.get_pdf(self.pdf_keys[0]).assert_is_valid_for_exp_data(data_exp)
+        self.get_pdf(self.pdf_keys[0]).assert_is_valid_for_trial_data(tdm=tdm)
 
-    def get_prob(self, tdm, gridfitparams):
+    def get_pd(self, tdm, gridparams, tl=None):
         """Calculates the signal energy probability (in logE) of each event for
         a given set of signal fit parameters on a grid.
 
@@ -208,22 +222,22 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
             probability should be calculated for. The following data fields must
             exist:
 
-            - 'log_energy' : float
-                The logarithm of the energy value of the event.
-            - 'sin_dec' : float
+            log_energy : float
+                The base-10 logarithm of the energy value of the event.
+            sin_dec : float
                 The sin(declination) value of the event.
 
-        gridfitparams : dict
+        gridparams : dict
             The dictionary holding the signal parameter values for which the
             signal energy probability should be calculated. Note, that the
             parameter values must match a set of parameter grid values for which
             an I3EnergyPDF object has been created at construction time of this
-            SignalI3EnergyPDF object. There is no interpolation method defined
-            at this point to allow for arbitrary parameter values!
+            SignalI3EnergyPDFSet instance. There is no interpolation method
+            defined at this point to allow for arbitrary parameter values!
 
         Returns
         -------
-        prob : 1d ndarray
+        pd : 1d ndarray
             The array with the signal energy probability for each event.
 
         Raises
@@ -231,7 +245,7 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
         KeyError
             If no energy PDF can be found for the given signal parameter values.
         """
-        i3energypdf = self.get_pdf(gridfitparams)
+        i3energypdf = self.get_pdf(gridparams)
 
-        prob = i3energypdf.get_prob(tdm)
-        return prob
+        pd = i3energypdf.get_pd(tdm)
+        return pd
