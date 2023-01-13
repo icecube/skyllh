d1da2b14c9c7744b597dded98405670d5529f978
Martin Wolf <mail@martin-wolf.org>
2023-01-13 16:31:16 +0100

    Adapt SignalI3EnergyPDFSet to new PDF interface

--- skyllh/i3/signalpdf.py
+++ skyllh/i3/signalpdf.py
@@ -2,44 +2,62 @@
 
 import numpy as np
 
-from skyllh.core.binning import BinningDefinition
+from skyllh.core.binning import (
+    BinningDefinition,
+)
 from skyllh.core.multiproc import (
     IsParallelizable,
-    parallelize
+    parallelize,
 )
 from skyllh.core.parameters import (
     ParameterGrid,
-    ParameterGridSet
+    ParameterGridSet,
 )
-from skyllh.core.smoothing import SmoothingFilter
+from skyllh.core.py import (
+    classname,
+)
+from skyllh.core.smoothing import (
+    SmoothingFilter,
+)
 from skyllh.core.pdf import (
     PDFSet,
-    IsSignalPDF
+    IsSignalPDF,
 )
-from skyllh.physics.flux_model import FluxModel
-from skyllh.physics.source_model import PointLikeSource
-from skyllh.i3.pdf import I3EnergyPDF
+from skyllh.physics.flux_model import (
+    FluxModel,
+)
+from skyllh.i3.pdf import (
+    I3EnergyPDF,
+)
 
 
 class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
     """This is the signal energy PDF for IceCube. It creates a set of
     I3EnergyPDF objects for a discrete set of energy signal parameters. Energy
-    signal parameters are the parameters that influence the source flux model.
+    signal parameters influence the source's flux model.
     """
-    def __init__(self, data_mc, logE_binning, sinDec_binning, fluxmodel,
-                 fitparam_grid_set, smoothing_filter=None, ncpu=None,
-                 ppbar=None):
+    def __init__(
+            self,
+            data_mc,
+            log10_energy_binning,
+            sin_dec_binning,
+            fluxmodel,
+            param_grid_set,
+            smoothing_filter=None,
+            ncpu=None,
+            ppbar=None,
+            **kwargs):
         """Creates a new IceCube energy signal PDF for a given flux model and
-        a set of fit parameter grids for the flux model.
+        a set of parameter grids for the flux model.
         It creates a set of I3EnergyPDF objects for each signal parameter value
-        permutation and stores it inside the ``_params_hash_I3EnergyPDF_dict``
-        dictionary, where the hash of the fit parameters dictionary is the key.
+        permutation and stores it in an internal dictionary, where the hash of
+        the parameters dictionary is the key.
 
         Parameters
         ----------
         data_mc : instance of DataFieldRecordArray
-            The array holding the monte-carlo data. The following data fields
-            must exist:
+            The instance of DataFieldRecordArray holding the monte-carlo data.
+            The following data fields must exist:
 
             true_energy : float
                 The true energy value of the data event.
@@ -52,200 +70,172 @@ class SignalI3EnergyPDFSet(PDFSet, IsSignalPDF, IsParallelizable):
                 The monte-carlo weight value of the data events in unit
                 GeV cm^2 sr.
 
-        logE_binning : BinningDefinition
-            The binning definition for the binning in log10(E).
-        sinDec_binning : BinningDefinition
-            The binning definition for the sin(declination).
-        fluxmodel : FluxModel
+        log10_energy_binning : instance of BinningDefinition
+            The binning definition for the reconstructed energy binning in
+            log10(E).
+        sin_dec_binning : instance of BinningDefinition
+            The binning definition for the binning in sin(declination).
+        fluxmodel : instance of FluxModel
             The flux model to use to create the signal energy PDF.
-        fitparam_grid_set : ParameterGridSet | ParameterGrid
-            The set of parameter grids. A ParameterGrid object for each
-            energy fit parameter, for which an I3EnergyPDF object needs to be
+        param_grid_set : instance of ParameterGridSet |
+                         instance of ParameterGrid
+            The set of parameter grids. A ParameterGrid instance for each
+            energy parameter, for which an I3EnergyPDF object needs to be
             created.
-        smoothing_filter : SmoothingFilter instance | None
+        smoothing_filter : instance of SmoothingFilter | None
             The smoothing filter to use for smoothing the energy histogram.
-            If None, no smoothing will be applied.
-        ncpu : int | None (default)
+            If ``None``, no smoothing will be applied.
+        ncpu : int | None
             The number of CPUs to use to create the different I3EnergyPDF
-            objects for the different fit parameter grid values.
-        ppbar : ProgressBar instance | None
+            instances for the different parameter grid values.
+            If set to ``None``, the configured default number of CPUs will be
+            used.
+        ppbar : instance of ProgressBar | None
             The instance of ProgressBar of the optional parent progress bar.
         """
-        if(isinstance(fitparam_grid_set, ParameterGrid)):
-            fitparam_grid_set = ParameterGridSet([fitparam_grid_set])
-        if(not isinstance(fitparam_grid_set, ParameterGridSet)):
-            raise TypeError('The fitparam_grid_set argument must be an '
-                'instance of ParameterGrid or ParameterGridSet!')
+        if isinstance(param_grid_set, ParameterGrid):
+            param_grid_set = ParameterGridSet([param_grid_set])
+        if not isinstance(param_grid_set, ParameterGridSet):
+            raise TypeError(
+                'The param_grid_set argument must be an instance of '
+                'ParameterGrid or ParameterGridSet! But its type is '
+                f'{classname(param_grid_set)}!')
 
-        # We need to extend the fit parameter grids on the lower and upper end
+        # We need to extend the parameter grids on the lower and upper end
         # by one bin to allow for the calculation of the interpolation. But we
         # will do this on a copy of the object.
-        fitparam_grid_set = fitparam_grid_set.copy()
-        fitparam_grid_set.add_extra_lower_and_upper_bin()
+        param_grid_set = param_grid_set.copy()
+        param_grid_set.add_extra_lower_and_upper_bin()
 
-        super(SignalI3EnergyPDFSet, self).__init__(pdf_type=I3EnergyPDF,
-            fitparams_grid_set=fitparam_grid_set, ncpu=ncpu)
+        super().__init__(
+            param_grid_set=param_grid_set,
+            ncpu=ncpu,
+            **kwargs)
 
-        if(not isinstance(logE_binning, BinningDefinition)):
-            raise TypeError('The logE_binning argument must be an instance of '
-                'BinningDefinition!')
-        if(not isinstance(sinDec_binning, BinningDefinition)):
-            raise TypeError('The sinDec_binning argument must be an instance '
-                'of BinningDefinition!')
-        if(not isinstance(fluxmodel, FluxModel)):
-            raise TypeError('The fluxmodel argument must be an instance of '
-                'FluxModel!')
-        if((smoothing_filter is not None) and
-           (not isinstance(smoothing_filter, SmoothingFilter))):
-            raise TypeError('The smoothing_filter argument must be None or '
-                'an instance of SmoothingFilter!')
+        if not isinstance(log10_energy_binning, BinningDefinition):
+            raise TypeError(
+                'The log10_energy_binning argument must be an instance of '
+                'BinningDefinition! '
+                f'Its type is {classname(log10_energy_binning)}!')
+        if not isinstance(sin_dec_binning, BinningDefinition):
+            raise TypeError(
+                'The sin_dec_binning argument must be an instance '
+                'of BinningDefinition! '
+                f'Its type is {classname(sin_dec_binning)}!')
+        if not isinstance(fluxmodel, FluxModel):
+            raise TypeError(
+                'The fluxmodel argument must be an instance of FluxModel! '
+                f'Its type is {classname(fluxmodel)}!')
+        if (smoothing_filter is not None) and\
+           (not isinstance(smoothing_filter, SmoothingFilter)):
+            raise TypeError(
+                'The smoothing_filter argument must be None or '
+                'an instance of SmoothingFilter! '
+                f'Its type is {classname(smoothing_filter)}!')
 
-        # Create I3EnergyPDF objects for all permutations of the fit parameter
+        # Create I3EnergyPDF objects for all permutations of the parameter
         # grid values.
         def create_I3EnergyPDF(
-            data_logE, data_sinDec, data_mcweight, data_true_energy,
-            logE_binning, sinDec_binning, smoothing_filter, fluxmodel,
-            gridfitparams):
+                data_log10_energy,
+                data_sin_dec,
+                data_mcweight,
+                data_true_energy,
+                log10_energy_binning,
+                sin_dec_binning,
+                smoothing_filter,
+                fluxmodel,
+                flux_unit_conv_factor,
+                gridparams):
             """Creates an I3EnergyPDF object for the given flux model and flux
             parameters.
 
             Parameters
             ----------
-            data_logE : 1d ndarray
-                The logarithm of the reconstructed energy value of the data
-                events.
-            data_sinDec : 1d ndarray
+            data_log10_energy : 1d ndarray
+                The base-10 logarithm of the reconstructed energy value of the
+                data events.
+            data_sin_dec : 1d ndarray
                 The sin(dec) value of the the data events.
             data_mcweight : 1d ndarray
                 The monte-carlo weight value of the data events.
             data_true_energy : 1d ndarray
                 The true energy value of the data events.
-            logE_binning : BinningDefinition
+            log10_energy_binning : instance of BinningDefinition
                 The binning definition for the binning in log10(E).
-            sinDec_binning : BinningDefinition
+            sin_dec_binning : instance of BinningDefinition
                 The binning definition for the sin(declination).
-            smoothing_filter : SmoothingFilter instance | None
+            smoothing_filter : instance of SmoothingFilter | None
                 The smoothing filter to use for smoothing the energy histogram.
-                If None, no smoothing will be applied.
-            fluxmodel : FluxModel
+                If ``None``, no smoothing will be applied.
+            fluxmodel : instance of FluxModel
                 The flux model to use to create the signal event weights.
-            gridfitparams : dict
+            flux_unit_conv_factor : float
+                The factor to convert the flux unit into the internal flux unit.
+            gridparams : dict
                 The dictionary holding the specific signal flux parameters.
 
             Returns
             -------
-            i3energypdf : I3EnergyPDF
-                The created I3EnergyPDF object for the given flux model and flux
-                parameters.
+            i3energypdf : instance of I3EnergyPDF
+                The created I3EnergyPDF instance for the given flux model and
+                flux parameters.
             """
             # Create a copy of the FluxModel with the given flux parameters.
             # The copy is needed to not interfer with other CPU processes.
-            myfluxmodel = fluxmodel.copy(newparams=gridfitparams)
+            myfluxmodel = fluxmodel.copy(newparams=gridparams)
 
             # Calculate the signal energy weight of the event. Note, that
             # because we create a normalized PDF, we can ignore all constants.
             # So we don't have to convert the flux unit into the internally used
             # flux unit.
-            data_physicsweight = myfluxmodel(data_true_energy)
+            data_physicsweight = np.squeeze(myfluxmodel(E=data_true_energy))
+            data_physicsweight *= flux_unit_conv_factor
 
             i3energypdf = I3EnergyPDF(
                 pmm=None,
-                data_log_energy=data_logE,
-                data_sin_dec=data_sinDec,
+                data_log10_energy=data_log10_energy,
+                data_sin_dec=data_sin_dec,
                 data_mcweight=data_mcweight,
                 data_physicsweight=data_physicsweight,
-                log_energy_binning=logE_binning,
-                sin_dec_binning=sinDec_binning,
+                log10_energy_binning=log10_energy_binning,
+                sin_dec_binning=sin_dec_binning,
                 smoothing_filter=smoothing_filter)
 
             return i3energypdf
 
-        data_logE = data_mc['log_energy']
-        data_sinDec = data_mc['sin_dec']
+        data_log10_energy = data_mc['log_energy']
+        data_sin_dec = data_mc['sin_dec']
         data_mcweight = data_mc['mcweight']
         data_true_energy = data_mc['true_energy']
 
+        flux_unit_conv_factor =\
+            fluxmodel.get_conversion_factor_to_internal_flux_unit()
+
         args_list = [
             (
-                (data_logE, data_sinDec, data_mcweight, data_true_energy,
-                 logE_binning, sinDec_binning, smoothing_filter,
-                 fluxmodel, gridfitparams),
+                (data_log10_energy,
+                 data_sin_dec,
+                 data_mcweight,
+                 data_true_energy,
+                 log10_energy_binning,
+                 sin_dec_binning,
+                 smoothing_filter,
+                 fluxmodel,
+                 flux_unit_conv_factor,
+                 gridparams),
                 {}
             )
-            for gridfitparams in self.gridfitparams_list
+            for gridparams in self.gridparams_list
         ]
 
         i3energypdf_list = parallelize(
-            create_I3EnergyPDF, args_list, self.ncpu, ppbar=ppbar)
+            func=create_I3EnergyPDF,
+            args_list=args_list,
+            ncpu=self.ncpu,
+            ppbar=ppbar)
 
-        # Save all the I3EnergyPDF objects in the IsSignalPDF PDF registry with
+        # Save all the I3EnergyPDF instances in the PDFSet registry with
         # the hash of the individual parameters as key.
-        for (gridfitparams, i3energypdf) in zip(self.gridfitparams_list,
-                                                i3energypdf_list):
-            self.add_pdf(i3energypdf, gridfitparams)
-
-    def assert_is_valid_for_trial_data(self, tdm):
-        """Checks if this signal energy PDF is valid for all the given trial
-        data.
-        It checks if all the data is within the log10(E) and sin(dec) binning
-        range.
-
-        Parameters
-        ----------
-        tdm : instance of TrialDataManager
-            The instance of TrialDataManager holding the trial data events.
-            The following data fields must exist:
-
-            log_energy : float
-                The base-10 logarithm of the energy value of the data event.
-            sin_dec : float
-                The sin(declination) of the data event.
-
-        Raises
-        ------
-        ValueError
-            If some of the data is outside the log10(E) or sin(dec) binning
-            range.
-        """
-        # Since we use the same binning for all the I3EnergyPDF objects, we
-        # can just use an arbitrary object to verify the data.
-        self.get_pdf(self.pdf_keys[0]).assert_is_valid_for_trial_data(tdm=tdm)
-
-    def get_pd(self, tdm, gridparams, tl=None):
-        """Calculates the signal energy probability (in logE) of each event for
-        a given set of signal fit parameters on a grid.
-
-        Parameters
-        ----------
-        tdm : instance of TrialDataManager
-            The TrialDataManager instance holding the data events for which the
-            probability should be calculated for. The following data fields must
-            exist:
-
-            log_energy : float
-                The base-10 logarithm of the energy value of the event.
-            sin_dec : float
-                The sin(declination) value of the event.
-
-        gridparams : dict
-            The dictionary holding the signal parameter values for which the
-            signal energy probability should be calculated. Note, that the
-            parameter values must match a set of parameter grid values for which
-            an I3EnergyPDF object has been created at construction time of this
-            SignalI3EnergyPDFSet instance. There is no interpolation method
-            defined at this point to allow for arbitrary parameter values!
-
-        Returns
-        -------
-        pd : 1d ndarray
-            The array with the signal energy probability for each event.
-
-        Raises
-        ------
-        KeyError
-            If no energy PDF can be found for the given signal parameter values.
-        """
-        i3energypdf = self.get_pdf(gridparams)
-
-        pd = i3energypdf.get_pd(tdm)
-        return pd
+        for (gridparams, i3energypdf) in zip(self.gridparams_list,
+                                             i3energypdf_list):
+            self.add_pdf(i3energypdf, gridparams)
