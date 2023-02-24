6fe37a1348f13028e459d6715107264068400150
Martin Wolf <mail@martin-wolf.org>
2023-02-24 17:36:57 +0100

    Adjust to new MultiDimGridPDF constructor

--- skyllh/core/utils/multidimgridpdf.py
+++ skyllh/core/utils/multidimgridpdf.py
@@ -5,94 +5,145 @@ MultiDimGridPDF instances.
 """
 
 import numpy as np
-import os
 
-from skyllh.core.binning import BinningDefinition
-from skyllh.core.pdf import MultiDimGridPDF
-from skyllh.core.signalpdf import SignalMultiDimGridPDF
-from skyllh.core.backgroundpdf import BackgroundMultiDimGridPDF
+from skyllh.core.binning import (
+    BinningDefinition,
+)
+from skyllh.core.pdf import (
+    MultiDimGridPDF,
+)
+from skyllh.core.signalpdf import (
+    SignalMultiDimGridPDF,
+)
+from skyllh.core.backgroundpdf import (
+    BackgroundMultiDimGridPDF,
+)
 
 
-def kde_pdf_sig_spatial_norm_factor_func(pdf, tdm, fitparams, eventdata):
-    """This is the standard normalization factor function for the spatial signal
-    MultiDimGridPDF, which is created from KDE PDF values.
+def get_kde_pdf_sig_spatial_norm_factor_func(
+        log10_psi_name='log10_psi'):
+    """Returns the standard normalization factor function for the spatial
+    signal MultiDimGridPDF, which is created from KDE PDF values.
     It can be used for the ``norm_factor_func`` argument of the
+    ``create_MultiDimGridPDF_from_photosplinetable`` and
     ``create_MultiDimGridPDF_from_kde_pdf`` function.
-    """
-    log10_psi_idx = pdf._axes.get_index_by_name('log10_psi')
-    # psi = tdm.get_data('psi')
-    # Convert to psi.
-    psi = 10**eventdata[:, log10_psi_idx]
-    norm = 1. / (2 * np.pi * np.log(10) * psi * np.sin(psi))
-    return norm
-
-
-def kde_pdf_bkg_norm_factor_func(pdf, tdm, fitparams, eventdata):
-    """This is the standard normalization factor function for the background
-    MultiDimGridPDF, which is created from KDE PDF values.
-    It can be used for the ``norm_factor_func`` argument of the
-    ``create_MultiDimGridPDF_from_kde_pdf`` function.
-    """
-    return 1. / (2 * np.pi)
-
-
-def create_MultiDimGridPDF_from_photosplinetable(
-        ds, data, info_key, splinetable_key, norm_factor_func=None,
-        kind=None, tl=None):
-    """
-    Creates a MultiDimGridPDF instance with pdf values taken from photospline pdf,
-    a spline interpolation of KDE PDF values stored in a splinetable on disk.
 
     Parameters
     ----------
-    ds : Dataset instance
-        The Dataset instance the PDF applies to.
-    data : DatasetData instance
-        The DatasetData instance that holds the auxiliary data of the data set.
+    log10_psi_name : str
+        The name of the event data field for the log10(psi) values.
+    """
+    def kde_pdf_sig_spatial_norm_factor_func(
+            pdf,
+            tdm,
+            params_recarray,
+            eventdata):
+
+        log10_psi_idx = pdf._axes.get_index_by_name(log10_psi_name)
+
+        psi = 10**eventdata[:, log10_psi_idx]
+
+        norm = 1. / (2 * np.pi * np.log(10) * psi * np.sin(psi))
+
+        return norm
+
+    return kde_pdf_sig_spatial_norm_factor_func
+
+
+def get_kde_pdf_bkg_norm_factor_func():
+    """Returns the standard normalization factor function for the background
+    MultiDimGridPDF, which is created from KDE PDF values.
+    It can be used for the ``norm_factor_func`` argument of the
+    ``create_MultiDimGridPDF_from_photosplinetable`` and
+    ``create_MultiDimGridPDF_from_kde_pdf`` function.
+    """
+    def kde_pdf_bkg_norm_factor_func(
+            pdf,
+            tdm,
+            params_recarray,
+            eventdata):
+
+        return 1. / (2 * np.pi)
+
+    return kde_pdf_bkg_norm_factor_func
+
+
+def create_MultiDimGridPDF_from_photosplinetable(
+        pmm,
+        ds,
+        data,
+        info_key,
+        splinetable_key,
+        kde_pdf_axis_name_map_key='KDE_PDF_axis_name_map',
+        norm_factor_func=None,
+        cache_pd_values=False,
+        kind=None,
+        tl=None):
+    """
+    Creates a MultiDimGridPDF instance with pdf values taken from a photospline
+    pdf, i.e. a spline interpolation of KDE PDF values stored in a splinetable
+    on disk.
+
+    Parameters
+    ----------
+    pmm : instance of ParameterModelMapper
+        The instance of ParameterModelMapper, which defines the mapping of
+        global parameters to local model parameters.
+    ds : instance of Dataset
+        The instance of Dataset the PDF applies to.
+    data : instance of DatasetData
+        The instance of DatasetData that holds the experimental and monte-carlo
+        data of the dataset.
     info_key : str
         The auxiliary data name for the file containing PDF information.
     splinetable_key : str
-        The auxiliary data name for the name of the splinetablefile.
+        The auxiliary data name for the name of the file containing the
+        photospline spline table.
+    kde_pdf_axis_name_map_key : str
+        The auxiliary data name for the KDE PDF axis name map.
     norm_factor_func : callable | None
-        The normalization factor function. It must have the following call
-        signature:
-
-            __call__(pdf, tdm, fitparams)
-
+        The function that calculates a possible required normalization
+        factor for the PDF value based on the event properties.
+        For more information about this argument see the documentation of the
+        :meth:`skyllh.core.pdf.MultiDimGridPDF.__init__` method.
+    cache_pd_values : bool
+        Flag if the probability density values should get cached by the
+        MultiDimGridPDF class.
     kind : str | None
         The kind of PDF to create. This is either ``'sig'`` for a
         SignalMultiDimGridPDF or ``'bkg'`` for a BackgroundMultiDimGridPDF
         instance. If set to None, a MultiDimGridPDF instance is created.
-    tl : TimeLord instance | None
-        The optional TimeLord instance to use for measuring timing information.
+    tl : instance of TimeLord | None
+        The optional instance of TimeLord to use for measuring timing
+        information.
 
     Returns
     -------
-    pdf : SignalMultiDimGridPDF instance | BackgroundMultiDimGridPDF instance |
-          MultiDimGridPDF instance
+    pdf : instance of SignalMultiDimGridPDF | instance of BackgroundMultiDimGridPDF | instance of MultiDimGridPDF
         The created PDF instance. Depending on the ``kind`` argument, this is
-        a SignalMultiDimGridPDF, a BackgroundMultiDimGridPDF, or a
-        MultiDimGridPDF instance.
+        a ``SignalMultiDimGridPDF``, a ``BackgroundMultiDimGridPDF``, or a
+        ``MultiDimGridPDF`` instance.
     """
 
-    if(kind is None):
-        pdf_type = MultiDimGridPDF
-    elif(kind == 'sig'):
-        pdf_type = SignalMultiDimGridPDF
-    elif(kind == 'bkg'):
-        pdf_type = BackgroundMultiDimGridPDF
+    if kind is None:
+        pdf_cls = MultiDimGridPDF
+    elif kind == 'sig':
+        pdf_cls = SignalMultiDimGridPDF
+    elif kind == 'bkg':
+        pdf_cls = BackgroundMultiDimGridPDF
     else:
-        raise ValueError('The kind argument must be None, "sig", or "bkg"! '
-                         'Currently it is '+str(kind)+'!')
+        raise ValueError(
+            'The kind argument must be None, "sig", or "bkg"! '
+            f'Currently it is {str(kind)}!')
 
     # Load the PDF data from the auxilary files.
     num_dict = ds.load_aux_data(info_key, tl=tl)
 
-    kde_pdf_axis_name_map = ds.load_aux_data('KDE_PDF_axis_name_map', tl=tl)
+    kde_pdf_axis_name_map = ds.load_aux_data(kde_pdf_axis_name_map_key, tl=tl)
     kde_pdf_axis_name_map_inv = dict(
         [(v, k) for (k, v) in kde_pdf_axis_name_map.items()])
     for var in num_dict['vars']:
-        if(var not in kde_pdf_axis_name_map_inv):
+        if var not in kde_pdf_axis_name_map_inv:
             kde_pdf_axis_name_map_inv[var] = var
 
     if 'bin_centers' in num_dict:
@@ -101,88 +152,106 @@ def create_MultiDimGridPDF_from_photosplinetable(
         bin_centers_key = 'bins'
     else:
         raise KeyError(
-            "The PDF information file is missing 'bin_centers' or 'bins' key.")
+            'The PDF information file is missing "bin_centers" or "bins" key!')
 
     axis_binnings = [
         BinningDefinition(
-            kde_pdf_axis_name_map_inv[var], num_dict[bin_centers_key][idx])
+            name=kde_pdf_axis_name_map_inv[var],
+            binedges=num_dict[bin_centers_key][idx])
         for (idx, var) in enumerate(num_dict['vars'])
     ]
 
     # Getting the name of the splinetable file
-    splinetable_file_list = ds.get_aux_data_definition(splinetable_key)
-    # This is a list with only one element.
-    splinetable_file = os.path.join(ds.root_dir, splinetable_file_list[0])
+    splinetable_file = ds.get_abs_pathfilename_list(
+        ds.get_aux_data_definition(splinetable_key))[0]
 
-    pdf = pdf_type(
-        axis_binnings,
+    pdf = pdf_cls(
+        pmm=pmm,
+        axis_binnings=axis_binnings,
         path_to_pdf_splinetable=splinetable_file,
-        pdf_grid_data=None,
-        norm_factor_func=norm_factor_func)
+        norm_factor_func=norm_factor_func,
+        cache_pd_values=cache_pd_values)
 
     return pdf
 
 
-def create_MultiDimGridPDF_from_kde_pdf(
-        ds, data, numerator_key, denumerator_key=None, norm_factor_func=None,
-        kind=None, tl=None):
+def create_MultiDimGridPDF_from_kde_pdf(  # noqa: C901
+        pmm,
+        ds,
+        data,
+        numerator_key,
+        denumerator_key=None,
+        kde_pdf_axis_name_map_key='KDE_PDF_axis_name_map',
+        norm_factor_func=None,
+        cache_pd_values=False,
+        kind=None,
+        tl=None):
     """Creates a MultiDimGridPDF instance with pdf values taken from KDE PDF
     values stored in the dataset's auxiliary data.
 
     Parameters
     ----------
-    ds : Dataset instance
-        The Dataset instance the PDF applies to.
-    data : DatasetData instance
-        The DatasetData instance that holds the auxiliary data of the data set.
+    pmm : instance of ParameterModelMapper
+        The instance of ParameterModelMapper, which defines the mapping of
+        global parameters to local model parameters.
+    ds : instance of Dataset
+        The instance of Dataset the PDF applies to.
+    data : instance of DatasetData
+        The instance of DatasetData that holds the auxiliary data of the
+        dataset.
     numerator_key : str
         The auxiliary data name for the PDF numerator array.
     denumerator_key : str | None
         The auxiliary data name for the PDF denumerator array.
-        This can be None, if no denumerator array is required.
+        This can be ``None``, if no denumerator array is required.
+    kde_pdf_axis_name_map_key : str
+        The auxiliary data name for the KDE PDF axis name map.
     norm_factor_func : callable | None
-        The normalization factor function. It must have the following call
-        signature:
-
-            __call__(pdf, tdm, fitparams)
-
+        The function that calculates a possible required normalization
+        factor for the PDF value based on the event properties.
+        For more information about this argument see the documentation of the
+        :meth:`skyllh.core.pdf.MultiDimGridPDF.__init__` method.
+    cache_pd_values : bool
+        Flag if the probability density values should get cached by the
+        MultiDimGridPDF class.
     kind : str | None
         The kind of PDF to create. This is either ``'sig'`` for a
         SignalMultiDimGridPDF or ``'bkg'`` for a BackgroundMultiDimGridPDF
         instance. If set to None, a MultiDimGridPDF instance is created.
-    tl : TimeLord instance | None
-        The optional TimeLord instance to use for measuring timing information.
+    tl : instance of TimeLord | None
+        The optional instance of TimeLord to use for measuring timing
+        information.
 
     Returns
     -------
-    pdf : SignalMultiDimGridPDF instance | BackgroundMultiDimGridPDF instance |
-          MultiDimGridPDF instance
+    pdf : instance of SignalMultiDimGridPDF | instance of BackgroundMultiDimGridPDF | instance of MultiDimGridPDF
         The created PDF instance. Depending on the ``kind`` argument, this is
-        a SignalMultiDimGridPDF, a BackgroundMultiDimGridPDF, or a
-        MultiDimGridPDF instance.
+        a ``SignalMultiDimGridPDF``, a ``BackgroundMultiDimGridPDF``, or a
+        ``MultiDimGridPDF`` instance.
     """
-    if(kind is None):
-        pdf_type = MultiDimGridPDF
-    elif(kind == 'sig'):
-        pdf_type = SignalMultiDimGridPDF
-    elif(kind == 'bkg'):
-        pdf_type = BackgroundMultiDimGridPDF
+    if kind is None:
+        pdf_cls = MultiDimGridPDF
+    elif kind == 'sig':
+        pdf_cls = SignalMultiDimGridPDF
+    elif kind == 'bkg':
+        pdf_cls = BackgroundMultiDimGridPDF
     else:
-        raise ValueError('The kind argument must be None, "sig", or "bkg"! '
-                         'Currently it is '+str(kind)+'!')
+        raise ValueError(
+            'The kind argument must be None, "sig", or "bkg"! '
+            f'Currently it is {str(kind)}!')
 
     # Load the PDF data from the auxilary files.
     num_dict = ds.load_aux_data(numerator_key, tl=tl)
 
     denum_dict = None
-    if(denumerator_key is not None):
+    if denumerator_key is not None:
         denum_dict = ds.load_aux_data(denumerator_key, tl=tl)
 
-    kde_pdf_axis_name_map = ds.load_aux_data('KDE_PDF_axis_name_map', tl=tl)
+    kde_pdf_axis_name_map = ds.load_aux_data(kde_pdf_axis_name_map_key, tl=tl)
     kde_pdf_axis_name_map_inv = dict(
         [(v, k) for (k, v) in kde_pdf_axis_name_map.items()])
     for var in num_dict['vars']:
-        if(var not in kde_pdf_axis_name_map_inv):
+        if var not in kde_pdf_axis_name_map_inv:
             kde_pdf_axis_name_map_inv[var] = var
 
     if 'bin_centers' in num_dict:
@@ -191,7 +260,7 @@ def create_MultiDimGridPDF_from_kde_pdf(
         bin_centers_key = 'bins'
     else:
         raise KeyError(
-            "The PDF information file is missing 'bin_centers' or 'bins' key.")
+            'The PDF information file is missing "bin_centers" or "bins" key!')
 
     axis_binnings = [
         BinningDefinition(
@@ -200,7 +269,7 @@ def create_MultiDimGridPDF_from_kde_pdf(
     ]
 
     vals = num_dict['pdf_vals']
-    if(denum_dict is not None):
+    if denum_dict is not None:
         # A denumerator is required, so we need to divide the numerator pdf
         # values by the denumerator pdf values, by preserving the correct axis
         # order.
@@ -208,7 +277,7 @@ def create_MultiDimGridPDF_from_kde_pdf(
         # match the axis order of the numerator pdf values array.
         selector = []
         for var in num_dict['vars']:
-            if(var in denum_dict['vars']):
+            if var in denum_dict['vars']:
                 # The variable is present in both pdf value arrays. So select
                 # all values of that dimension.
                 selector.append(slice(None, None))
@@ -228,10 +297,11 @@ def create_MultiDimGridPDF_from_kde_pdf(
     # Set NaN values to 0.
     vals[np.isnan(vals)] = 0
 
-    pdf = pdf_type(
-        axis_binnings,
-        path_to_pdf_splinetable=None,
+    pdf = pdf_cls(
+        pmm=pmm,
+        axis_binnings=axis_binnings,
         pdf_grid_data=vals,
-        norm_factor_func=norm_factor_func)
+        norm_factor_func=norm_factor_func,
+        cache_pd_values=cache_pd_values)
 
     return pdf
