9f865ae8e7d02847912e8c584f132a59d4b2bb52
Tomas Kontrimas <t.kontrimas@outlook.com>
2023-04-12 11:53:34 +0200

    Remove unused imports

--- skyllh/core/analysis.py
+++ skyllh/core/analysis.py
@@ -5,7 +5,6 @@
 
 import abc
 import numpy as np
-import pickle
 
 from skyllh.core.py import (
     classname,
@@ -15,17 +14,12 @@ from skyllh.core.debugging import get_logger
 from skyllh.core.storage import DataFieldRecordArray
 from skyllh.core.dataset import (
     Dataset,
-    DatasetData
+    DatasetData,
 )
 from skyllh.core.parameters import (
     FitParameter,
     SourceFitParameterMapper,
-    SingleSourceFitParameterMapper
-)
-from skyllh.core.pdf import (
-    EnergyPDF,
-    SpatialPDF,
-    TimePDF
+    SingleSourceFitParameterMapper,
 )
 from skyllh.core.pdfratio import PDFRatio
 from skyllh.core.progressbar import ProgressBar
@@ -36,15 +30,11 @@ from skyllh.core.llhratio import (
     SingleSourceDatasetSignalWeights,
     SingleSourceZeroSigH0SingleDatasetTCLLHRatio,
     MultiSourceZeroSigH0SingleDatasetTCLLHRatio,
-    MultiSourceDatasetSignalWeights
+    MultiSourceDatasetSignalWeights,
 )
-from skyllh.core.scrambling import DataScramblingMethod
 from skyllh.core.timing import TaskTimer
 from skyllh.core.trialdata import TrialDataManager
-from skyllh.core.optimize import (
-    EventSelectionMethod,
-    AllEventSelectionMethod
-)
+from skyllh.core.optimize import EventSelectionMethod
 from skyllh.core.source_hypothesis import SourceHypoGroupManager
 from skyllh.core.test_statistic import TestStatistic
 from skyllh.core.multiproc import get_ncpu, parallelize
@@ -52,7 +42,7 @@ from skyllh.core.background_generation import BackgroundGenerationMethod
 from skyllh.core.background_generator import BackgroundGenerator
 from skyllh.core.signal_generator import (
     SignalGeneratorBase,
-    SignalGenerator
+    SignalGenerator,
 )
 from skyllh.physics.source import SourceModel
 
