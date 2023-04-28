7f4b7903932ad138e4d58885f74e77aa88da1934
Martin Wolf <mail@martin-wolf.org>
2023-04-28 20:18:23 +0200

    Comply with flake8

--- skyllh/core/math.py
+++ skyllh/core/math.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
-"""The `math` module contains classes for pure mathematical objects.
+"""The ``math`` module contains classes for pure mathematical objects.
 """
 
-from __future__ import division
-
 import abc
-from copy import deepcopy
+from copy import (
+    deepcopy,
+)
 
 from skyllh.core.py import (
     classname,
     isproperty,
     issequence,
-    issequenceof
+    issequenceof,
 )
 
 
-class MathFunction(object, metaclass=abc.ABCMeta):
+class MathFunction(
+        object,
+        metaclass=abc.ABCMeta):
     """This abstract base class provides an implementation for a mathematical
     function. Such a function has defined parameters, which are implemented as
     class properties. The tuple of parameter names is defined through the
@@ -33,23 +35,26 @@ class MathFunction(object, metaclass=abc.ABCMeta):
         """The tuple holding the names of the math function's parameters.
         """
         return self._param_names
+
     @param_names.setter
     def param_names(self, names):
-        if(not issequence(names)):
+        if not issequence(names):
             names = (names,)
-        if(not issequenceof(names, str)):
-            raise TypeError('The param_names property must be a sequence of '
-                'str!')
+        if not issequenceof(names, str):
+            raise TypeError(
+                'The param_names property must be a sequence of str!')
         names = tuple(names)
         # Check if all the given names are actual properties of this
         # MathFunction class.
         for name in names:
-            if(not hasattr(self, name)):
-                raise KeyError('The "%s" class does not have an attribute '
-                    'named "%s"!'%(classname(self), name))
-            if(not isproperty(self, name)):
-                raise TypeError('The attribute "%s" of "%s" is not a '
-                    'property!'%(classname(self), name))
+            if not hasattr(self, name):
+                raise KeyError(
+                    f'The "{classname(self)}" class does not have an attribute '
+                    f'named "{name}"!')
+            if not isproperty(self, name):
+                raise TypeError(
+                    f'The attribute "{classname(self)}" of "{name}" is not a '
+                    'property!')
         self._param_names = names
 
     @property
@@ -64,7 +69,9 @@ class MathFunction(object, metaclass=abc.ABCMeta):
         """
         return self.math_function_str
 
-    def copy(self, newparams=None):
+    def copy(
+            self,
+            newparams=None):
         """Copies this MathFunction object by calling the copy.deepcopy
         function, and sets new parameters if requested.
 
@@ -78,12 +85,14 @@ class MathFunction(object, metaclass=abc.ABCMeta):
         f = deepcopy(self)
 
         # Set the new parameter values.
-        if(newparams is not None):
+        if newparams is not None:
             f.set_params(newparams)
 
         return f
 
-    def set_params(self, pdict):
+    def set_params(
+            self,
+            pdict):
         """Sets the parameters of the math function to the given parameter
         values.
 
@@ -98,15 +107,16 @@ class MathFunction(object, metaclass=abc.ABCMeta):
         updated : bool
             Flag if parameter values were actually updated.
         """
-        if(not isinstance(pdict, dict)):
-            raise TypeError('The pdict argument must be of type dict!')
+        if not isinstance(pdict, dict):
+            raise TypeError(
+                'The pdict argument must be of type dict!')
 
         updated = False
 
         for pname in self._param_names:
             current_value = getattr(self, pname)
             pvalue = pdict.get(pname, current_value)
-            if(pvalue != current_value):
+            if pvalue != current_value:
                 setattr(self, pname, pvalue)
                 updated = True
 
