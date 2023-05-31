2a358c280da3d61b509b07fc7a25e2eb6d0d7589
Martin Wolf <mail@martin-wolf.org>
2023-05-31 18:27:07 +0200

    Fix source_model unit tests

--- tests/physics/test_source.py
+++ tests/physics/test_source.py
@@ -1,90 +1,108 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import numpy as np
 
-from skyllh.physics.source import (
-    SourceLocation,
-    SourceModel,
-    SourceCollection,
-    Catalog,
+from skyllh.core.catalog import (
+    SourceCatalog,
+)
+from skyllh.physics.source_model import (
     PointLikeSource,
-    PointLikeSourceCollection,
-    PointLikeSourceCatalog
+    SourceModel,
+    SourceModelCollection,
 )
 
 
-class TestSource(unittest.TestCase):
+class SourceModelTestCase(
+        unittest.TestCase
+):
+    def setUp(self):
+        self.name = 'MySource'
+        self.classification = 'MyClassification'
+        self.weight = 1.1
+
+        self.source_model = SourceModel(
+            name=self.name,
+            classification=self.classification,
+            weight=self.weight)
+
+    def test_name(self):
+        self.assertEqual(self.source_model.name, self.name)
+
+    def test_classification(self):
+        self.assertEqual(self.source_model.classification, self.classification)
+
+    def test_weight(self):
+        self.assertEqual(self.source_model.weight, self.weight)
+
+
+class SourceModelCollectionTestCase(
+        unittest.TestCase,
+):
     def setUp(self):
         self.ra = 0
         self.dec = 1
 
-    def test_SourceLocation(self):
-        source_location = SourceLocation(self.ra, self.dec)
-
-        self.assertEqual(source_location.ra, self.ra)
-        self.assertEqual(source_location.dec, self.dec)
-
-    def test_SourceModel(self):
-        source_model = SourceModel(self.ra, self.dec)
-
-        self.assertEqual(source_model.loc.ra, self.ra)
-        self.assertEqual(source_model.loc.dec, self.dec)
-
-    def test_SourceCollection(self):
+    def test_SourceModelCollection(self):
         source_model1 = SourceModel(self.ra, self.dec)
         source_model2 = SourceModel(self.ra, self.dec)
 
-        source_collection_casted = SourceCollection.cast(source_model1, "Could not cast SourceModel to SourceCollection")
-        source_collection = SourceCollection(source_type=SourceModel, sources=[source_model1, source_model2])
+        source_collection_casted = SourceModelCollection.cast(
+            source_model1,
+            "Could not cast SourceModel to SourceCollection")
+        source_collection = SourceModelCollection(
+            source_type=SourceModel,
+            sources=[source_model1, source_model2])
 
-        self.assertIsInstance(source_collection_casted, SourceCollection)
+        self.assertIsInstance(source_collection_casted, SourceModelCollection)
         self.assertEqual(source_collection.source_type, SourceModel)
         self.assertIsInstance(source_collection.sources[0], SourceModel)
         self.assertIsInstance(source_collection.sources[1], SourceModel)
 
-    def test_Catalog(self):
-        name = "Catalog test"
-        source_model1 = SourceModel(self.ra, self.dec)
-        source_model2 = SourceModel(self.ra, self.dec)
 
-        catalog = Catalog(name, source_type=SourceModel, sources=[source_model1, source_model2])
-        source_collection_from_catalog = catalog.as_source_collection()
+class SourceCatalogTestCase(
+        unittest.TestCase,
+):
+    def setUp(self):
+        self.name = 'MySourceCatalog'
+        self.ra = 0.1
+        self.dec = 1.1
+        self.source1 = SourceModel(self.ra, self.dec)
+        self.source2 = SourceModel(self.ra, self.dec)
 
-        self.assertEqual(catalog.name, name)
-        self.assertIsInstance(source_collection_from_catalog, SourceCollection)
+        self.catalog = SourceCatalog(
+            name=self.name,
+            sources=[self.source1, self.source2],
+            source_type=SourceModel)
 
-    def test_PointLikeSource(self):
-        point_like_source = PointLikeSource(self.ra, self.dec)
+    def test_name(self):
+        self.assertEqual(self.catalog.name, self.name)
 
-        self.assertEqual(point_like_source.ra, self.ra)
-        self.assertEqual(point_like_source.dec, self.dec)
-
-    def test_PointLikeSourceCollection(self):
-        point_like_source1 = PointLikeSource(self.ra, self.dec)
-        point_like_source2 = PointLikeSource(self.ra, self.dec)
-
-        point_like_source_collection = PointLikeSourceCollection(sources=[point_like_source1, point_like_source2])
-        ra_array = np.array([self.ra, self.ra])
-        dec_array = np.array([self.dec, self.dec])
-
-        self.assertIsInstance(point_like_source_collection.sources[0], SourceModel)
-        self.assertIsInstance(point_like_source_collection.sources[1], SourceModel)
-        np.testing.assert_array_equal(point_like_source_collection.ra, ra_array)
-        np.testing.assert_array_equal(point_like_source_collection.dec, dec_array)
-
-    def test_PointLikeSourceCatalog(self):
-        name = "Point like source catalog test"
-        point_like_source1 = PointLikeSource(self.ra, self.dec)
-        point_like_source2 = PointLikeSource(self.ra, self.dec)
-
-        point_like_source_catalog = PointLikeSourceCatalog(name, sources=[point_like_source1, point_like_source2])
-
-        self.assertEqual(point_like_source_catalog.name, name)
-        self.assertEqual(point_like_source_catalog.source_type, PointLikeSource)
-        self.assertIsInstance(point_like_source_catalog.sources[0], PointLikeSource)
-        self.assertIsInstance(point_like_source_catalog.sources[1], PointLikeSource)
+    def test_as_SourceModelCollection(self):
+        sc = self.catalog.as_SourceModelCollection()
+        self.assertIsInstance(sc, SourceModelCollection)
 
 
-if(__name__ == '__main__'):
+class PointLikeSourceTestCase(
+        unittest.TestCase,
+):
+    def setUp(self):
+        self.name = 'MyPointLikeSource'
+        self.ra = 0.1
+        self.dec = 1.1
+        self.source = PointLikeSource(
+            name=self.name,
+            ra=self.ra,
+            dec=self.dec)
+
+    def test_name(self):
+        self.assertEqual(self.source.name, self.name)
+
+    def test_ra(self):
+        self.assertEqual(self.source.ra, self.ra)
+
+    def test_dec(self):
+        self.assertEqual(self.source.dec, self.dec)
+
+
+if __name__ == '__main__':
     unittest.main()
