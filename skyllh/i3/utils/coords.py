2e88c1434a3e65a043e05bfb9cde819902f9e8e2
Martin Wolf <mail@martin-wolf.org>
2023-06-08 10:59:26 +0200

    Comply with flake8

--- skyllh/i3/utils/coords.py
+++ skyllh/i3/utils/coords.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 
+
 def azi_to_ra_transform(azi, mjd):
     """Rotates the given IceCube azimuth angles into right-ascention angles for
     the given MJD times. This function is IceCube specific and assumes that the
@@ -13,31 +14,46 @@ def azi_to_ra_transform(azi, mjd):
 
     Parameters
     ----------
-    azi : ndarray
+    azi : instance of numpy.ndarray
         The array with the azimuth angles.
-    mjd : ndarray
+    mjd : instance of numpy.ndarray
         The array with the MJD times for each azimuth angle.
+
+    Returns
+    -------
+    ra : instance of numpy.ndarray
+        The right-ascention values.
     """
-    _sidereal_length = 0.997269566 # sidereal day = length * solar day
-    _sidereal_offset = 2.54199002505 # RA = offset + 2pi * (MJD/sidereal_length)%1 - azimuth
-    sidereal_day_residuals = ((mjd/_sidereal_length)%1)
+    # sidereal day = length * solar day
+    _sidereal_length = 0.997269566
+    _sidereal_offset = 2.54199002505
+    sidereal_day_residuals = (mjd / _sidereal_length) % 1
     ra = _sidereal_offset + 2 * np.pi * sidereal_day_residuals - azi
     ra = np.mod(ra, 2*np.pi)
+
     return ra
+
 
 def ra_to_azi_transform(ra, mjd):
     """Rotates the given right-ascention angles to local IceCube azimuth angles.
 
     Parameters
     ----------
-    ra : ndarray
+    ra : instance of numpy.ndarray
         The array with the right-ascention angles.
-    mjd : ndarray
+    mjd : instance of numpy.ndarray
         The array with the MJD times for each right-ascention angle.
 
+    Returns
+    -------
+    azi : instance of numpy.ndarray
+        The azimuth angle for each right-ascention angle.
     """
     # Use the azi_to_ra_transform function because it is symmetric.
-    return azi_to_ra_transform(ra, mjd)
+    azi = azi_to_ra_transform(ra, mjd)
+
+    return azi
+
 
 def hor_to_equ_transform(azi, zen, mjd):
     """Transforms the coordinate from the horizontal system (azimuth, zenith)
@@ -47,18 +63,19 @@ def hor_to_equ_transform(azi, zen, mjd):
 
     Parameters
     ----------
-    azi : ndarray
+    azi : instance of numpy.ndarray
         The azimuth angle.
-    zen : ndarray
+    zen : instance of numpy.ndarray
         The zenith angle.
-    mjd : ndarray
+    mjd : instance of numpy.ndarray
         The time in MJD.
 
     Returns
     -------
-    (ra, dec) : (ndarray, ndarray)
-        The two-element tuple with the arrays of right-ascention and
-        declination.
+    ra : instance of numpy.ndarray
+        The right-ascention angle.
+    dec : instance of numpy.ndarray
+        The declination angle.
     """
     ra = azi_to_ra_transform(azi, mjd)
     dec = np.pi - zen
