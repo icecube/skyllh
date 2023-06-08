423bfe3e2ff63a840ea00e2978652ba263776f67
Martin Wolf <mail@martin-wolf.org>
2023-06-08 11:21:12 +0200

    Add from_grl_data unittest

--- tests/i3/test_livetime.py
+++ tests/i3/test_livetime.py
@@ -3,6 +3,9 @@
 import os.path
 import unittest
 
+from skyllh.core.storage import (
+    create_FileLoader,
+)
 from skyllh.i3.dataset import (
     I3Dataset,
 )
@@ -11,7 +14,7 @@ from skyllh.i3.livetime import (
 )
 
 
-class TestI3Livetime(
+class I3LivetimeTestCase(
         unittest.TestCase
 ):
     """Test I3Livetime class.
@@ -24,6 +27,17 @@ class TestI3Livetime(
         self.livetime_total = 1.6666178000014042
         self.n_uptime_mjd_intervals = 5
 
+    def test_from_grl_data(self):
+        grl_fileloader = create_FileLoader(self.grl_pathfilenames)
+        grl_data = grl_fileloader.load_data()
+
+        i3livetime = I3Livetime.from_grl_data(grl_data)
+
+        self.assertEqual(
+            i3livetime.livetime, self.livetime_total)
+        self.assertEqual(
+            i3livetime.n_uptime_mjd_intervals, self.n_uptime_mjd_intervals)
+
     def test_from_grl_files(self):
         i3livetime = I3Livetime.from_grl_files(self.grl_pathfilenames)
 
@@ -35,7 +49,7 @@ class TestI3Livetime(
     def test_from_I3Dataset(self):
         # Create a test data set.
         i3dataset = I3Dataset(
-            name='I3Dataset_test',
+            name='test_dataset',
             exp_pathfilenames=self.exp_pathfilenames,
             mc_pathfilenames=self.mc_pathfilenames,
             grl_pathfilenames=self.grl_pathfilenames,
