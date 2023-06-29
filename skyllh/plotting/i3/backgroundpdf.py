d709d89e42aa5ecd71443b1c8be4f0ee3c7a3c09
Martin Wolf <mail@martin-wolf.org>
2023-06-29 17:29:02 +0200

    Comply with flake8

--- skyllh/plotting/i3/backgroundpdf.py
+++ skyllh/plotting/i3/backgroundpdf.py
@@ -2,18 +2,31 @@
 
 """Plotting module to plot IceCube specific background PDF objects.
 """
+
 import numpy as np
 
-from matplotlib.axes import Axes
-from matplotlib.colors import LogNorm
+from matplotlib.axes import (
+    Axes,
+)
+from matplotlib.colors import (
+    LogNorm,
+)
 
-from skyllh.core.py import classname
+from skyllh.core.py import (
+    classname,
+)
 from skyllh.core.source_hypo_grouping import (
     SourceHypoGroupManager,
 )
-from skyllh.core.storage import DataFieldRecordArray
-from skyllh.core.trialdata import TrialDataManager
-from skyllh.i3.backgroundpdf import BackgroundI3SpatialPDF
+from skyllh.core.storage import (
+    DataFieldRecordArray,
+)
+from skyllh.core.trialdata import (
+    TrialDataManager,
+)
+from skyllh.i3.backgroundpdf import (
+    BackgroundI3SpatialPDF,
+)
 
 
 class BackgroundI3SpatialPDFPlotter(object):
@@ -39,10 +52,13 @@ class BackgroundI3SpatialPDFPlotter(object):
         """The PDF object to plot.
         """
         return self._pdf
+
     @pdf.setter
     def pdf(self, pdf):
-        if(not isinstance(pdf, BackgroundI3SpatialPDF)):
-            raise TypeError('The pdf property must be an object of instance BackgroundI3SpatialPDF!')
+        if not isinstance(pdf, BackgroundI3SpatialPDF):
+            raise TypeError(
+                'The pdf property must be an object of instance '
+                'BackgroundI3SpatialPDF!')
         self._pdf = pdf
 
     @property
@@ -50,11 +66,12 @@ class BackgroundI3SpatialPDFPlotter(object):
         """The TrialDataManager that provides the data for the PDF evaluation.
         """
         return self._tdm
+
     @tdm.setter
     def tdm(self, obj):
-        if(not isinstance(obj, TrialDataManager)):
-            raise TypeError('The tdm property must be an instance of '
-                'TrialDataManager!')
+        if not isinstance(obj, TrialDataManager):
+            raise TypeError(
+                'The tdm property must be an instance of TrialDataManager!')
         self._tdm = obj
 
     def plot(self, src_hypo_group_manager, axes):
@@ -74,11 +91,13 @@ class BackgroundI3SpatialPDFPlotter(object):
         img : instance of mpl.AxesImage
             The AxesImage instance showing the PDF image.
         """
-        if(not isinstance(src_hypo_group_manager, SourceHypoGroupManager)):
-            raise TypeError('The src_hypo_group_manager argument must be an '
+        if not isinstance(src_hypo_group_manager, SourceHypoGroupManager):
+            raise TypeError(
+                'The src_hypo_group_manager argument must be an '
                 'instance of SourceHypoGroupManager!')
-        if(not isinstance(axes, Axes)):
-            raise TypeError('The axes argument must be an instance of '
+        if not isinstance(axes, Axes):
+            raise TypeError(
+                'The axes argument must be an instance of '
                 'matplotlib.axes.Axes!')
 
         # By construction the BackgroundI3SpatialPDF does not depend on
@@ -88,8 +107,9 @@ class BackgroundI3SpatialPDFPlotter(object):
         pdfprobs = np.zeros((1, sin_dec_binning.nbins))
 
         sin_dec_points = sin_dec_binning.bincenters
-        events = DataFieldRecordArray(np.zeros((pdfprobs.size,),
-                          dtype=[('sin_dec', np.float64)]))
+        events = DataFieldRecordArray(np.zeros(
+            (pdfprobs.size,),
+            dtype=[('sin_dec', np.float64)]))
         for (i, sin_dec) in enumerate(sin_dec_points):
             events['sin_dec'][i] = sin_dec
 
@@ -98,14 +118,18 @@ class BackgroundI3SpatialPDFPlotter(object):
         event_probs = self._pdf.get_prob(self._tdm)
 
         for i in range(len(events)):
-            pdfprobs[0,i] = event_probs[i]
+            pdfprobs[0, i] = event_probs[i]
 
         ra_axis = self.pdf.axes['ra']
         (left, right, bottom, top) = (
             ra_axis.vmin, ra_axis.vmax,
             sin_dec_binning.lower_edge, sin_dec_binning.upper_edge)
-        img = axes.imshow(pdfprobs.T, extent=(left, right, bottom, top), origin='lower',
-                    norm=LogNorm(), interpolation='none')
+        img = axes.imshow(
+            pdfprobs.T,
+            extent=(left, right, bottom, top),
+            origin='lower',
+            norm=LogNorm(),
+            interpolation='none')
         axes.set_xlabel('ra')
         axes.set_ylabel('sin_dec')
         axes.set_title(classname(self.pdf))
