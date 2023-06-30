57ad454e35752a925f65bc14aa2c8061d375df4a
Martin Wolf <mail@martin-wolf.org>
2023-06-30 10:42:50 +0200

    Update example

--- examples/scrambling.py
+++ examples/scrambling.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Example how to use the data scrambling mechanism of skyllh.
+Example how to use the data scrambling mechanism of SkyLLH.
 """
 
 import numpy as np
 
+from skyllh.core.livetime import (
+    Livetime,
+)
 from skyllh.core.random import (
     RandomStateService,
 )
@@ -13,11 +16,28 @@ from skyllh.core.scrambling import (
     DataScrambler,
     UniformRAScramblingMethod,
 )
+from skyllh.core.times import (
+    LivetimeTimeGenerationMethod,
+    TimeGenerator,
+)
+
+from skyllh.i3.scrambling import (
+    I3TimeScramblingMethod,
+)
 
 
-def gen_data(rss, N=100, window=(0, 365)):
-    """Create uniformly distributed data on sphere. """
-    arr = np.empty((N,), dtype=[("ra", np.float64), ("dec", np.float64)])
+def gen_data(rss, N):
+    """Create uniformly distributed data on sphere.
+    """
+    arr = np.empty(
+        (N,),
+        dtype=[
+            ("azi", np.float64),
+            ("zen", np.float64),
+            ("ra", np.float64),
+            ("dec", np.float64),
+            ("time", np.float64),
+        ])
 
     arr["ra"] = rss.random.uniform(0., 2.*np.pi, N)
     arr["dec"] = rss.random.uniform(-np.pi, np.pi, N)
@@ -25,18 +45,67 @@ def gen_data(rss, N=100, window=(0, 365)):
     return arr
 
 
-if __name__ == '__main__':
+def ex1():
+    """Data scrambling via right-ascention scrambling.
+    """
+    print("Example 1")
+    print("=========")
+
     rss = RandomStateService(seed=1)
 
     # Generate some psydo data.
-    data = gen_data(rss, N=10)
-    print(data['ra'])
+    data = gen_data(rss=rss, N=10)
+    print(f'before scrambling: data["ra"]={data["ra"]}')
 
     # Create DataScrambler instance with uniform RA scrambling.
-    scr = DataScrambler(method=UniformRAScramblingMethod())
+    scrambler = DataScrambler(
+        method=UniformRAScramblingMethod())
 
     # Scramble the data.
-    scr.scramble_data(
+    scrambler.scramble_data(
         rss=rss,
+        dataset=None,
         data=data)
-    print(data['ra'])
+
+    print(f'after scrambling: data["ra"]={data["ra"]}')
+
+
+def ex2():
+    """Data scrambling via detector on-time scrambling.
+    """
+    print("Example 2")
+    print("=========")
+
+    rss = RandomStateService(seed=1)
+
+    # Generate some psydo data.
+    data = gen_data(rss=rss, N=10)
+    print(f'before scrambling: data["ra"]={data["ra"]}')
+
+    # Create a Livetime object, which defines the detector live-time.
+    lt = Livetime(uptime_mjd_intervals_arr=np.array(
+        [
+            [55000, 56000],
+            [60000, 69000]
+        ],
+        dtype=np.float64))
+
+    # Create a TimeGenerator with an on-time time generation method.
+    timegen = TimeGenerator(method=LivetimeTimeGenerationMethod(livetime=lt))
+
+    # Create DataScrambler with IceCube time scrambing method.
+    scrambler = DataScrambler(
+        method=I3TimeScramblingMethod(timegen))
+
+    # Scramble the data.
+    scrambler.scramble_data(
+        rss=rss,
+        dataset=None,
+        data=data)
+
+    print(f'after scrambling: data["ra"]={data["ra"]}')
+
+
+if __name__ == '__main__':
+    ex1()
+    ex2()
