45e443768f0ed3fc855d1246bb6992dcce3316cb
Martin Wolf <mail@martin-wolf.org>
2023-07-10 17:38:49 +0200

    Add DataFieldStage_t class

--- skyllh/core/types.py
+++ skyllh/core/types.py
@@ -5,6 +5,38 @@ circular imports when actively checking for types.
 """
 
 
+class DataFieldStages_t(
+    object,
+):
+
+    EXP_DATAFILE = 1
+    MC_DATAFILE = 2
+    EXP_DATAPREPARATION = 4
+    MC_DATAPREPARATION = 8
+    EXP_ANALYSIS = 16
+    MC_ANALYSIS = 32
+
+    @staticmethod
+    def and_check(stage, stages):
+        """Checks if the given stage matches all of the given stages.
+        """
+        for stage_ in stages:
+            if stage & stage_ == 0:
+                return False
+
+        return True
+
+    @staticmethod
+    def or_check(stage, stages):
+        """Checks if the given stage matches at least one of the given stages.
+        """
+        for stage_ in stages:
+            if stage & stage_ != 0:
+                return True
+
+        return False
+
+
 class SourceHypoGroup_t(
     object,
 ):
