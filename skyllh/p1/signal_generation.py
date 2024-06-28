d81963eb3159fb24e4f96870d93f70397a67b45b
Jean Pierre Twagirayezu <twagirapeter1@gmail.com>
2024-06-28 08:29:44 -0400

    cleaning up

--- skyllh/p1/signal_generation.py
+++ skyllh/p1/signal_generation.py
@@ -95,7 +95,8 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
         Parameters
         ----------
         band_deg_alt_range : float
-            The withd of the altitude bands in degree.
+            The width of the alt band to take MC events from around a
+            source altitude position in AltAz coordinates.
         energy_range : 2-element tuple of float | None
             The energy range from which to take MC events into account for
             signal event generation.
@@ -108,8 +109,23 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
             energy_range=energy_range,
             **kwargs)
 
-        self.src_batch_size = src_batch_size
         self.band_deg_alt_range = band_deg_alt_range
+        self.src_batch_size = src_batch_size
+
+        @property
+        def band_deg_alt_range(self):
+            """The width of the alt band to take MC events from around a
+            source altitude position in AltAz coordinates.
+            """
+            return self._band_deg_alt_range
+
+        @band_deg_alt_range.setter
+        def band_deg_alt_range(self, v):
+            v = float_cast(
+                v,
+                'The band_deg_alt_range property must be cast-able to type '
+                'float!')
+            self._band_deg_alt_range = v
 
         @property
         def src_batch_size(self):
@@ -126,6 +142,30 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
             self._src_batch_size = v
 
     def calc_source_signal_mc_event_flux(self, data_mc, shg):
+        """Calculates the signal flux of each given MC event for each source
+        hypothesis of the given source hypothesis group.
+
+        Parameters
+        ----------
+        data_mc : numpy record ndarray
+            The numpy record array holding the MC events of a dataset.
+        shg : SourceHypoGroup instance
+            The source hypothesis group, which defines the list of sources, and
+            their flux model.
+
+        Returns
+        -------
+        ev_idx_arr : ndarray
+            The (N_selected_signal_events,)-shaped 1D ndarray holding the index
+            of the MC event.
+        shg_src_idx_arr : ndarray
+            The (N_selected_signal_events,)-shaped 1D ndarray holding the index
+            of the source within the given source hypothesis group for each
+            signal candidate event.
+        flux_arr : ndarray
+            The (N_selected_signal_events,)-shaped 1D ndarray holding the flux
+            value of each signal candidate event.
+        """
         indices = np.arange(
             0, len(data_mc),
             dtype=get_smallest_numpy_int_type((0, len(data_mc)))
@@ -145,6 +185,17 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
         
         data_mc_sin_true_alt = data_mc['sin_true_alt']
         data_mc_true_energy = data_mc['true_energy']
+
+        alt = np.array([0.5969206])  # fake data
+
+        # Calculate the source altitude bands and their solid angle.
+        band_deg_alt_range = self.band_deg_alt_range  # self._band_deg_alt_range does not work!
+        src_sin_alt_band_min = np.sin(np.deg2rad(np.rad2deg(alt) - band_deg_alt_range/2))
+        src_sin_alt_band_max = np.sin(np.deg2rad(np.rad2deg(alt) + band_deg_alt_range/2))
+
+        src_alt_band_omega = (
+            2 * np.pi * (src_sin_alt_band_max - src_sin_alt_band_min)
+        )
 
         # Get the flux model of this source hypo group (SHG).
         fluxmodel = shg.fluxmodel
@@ -168,10 +219,8 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
             (0,),
             dtype=np.float32)
         
-        src_batch_size = self.src_batch_size
+        src_batch_size = self.src_batch_size  # self._src_batch_size does not work!
         n_batches = int(np.ceil(n_sources / src_batch_size))
-
-        alt = np.array([0.5969206])  # fake data
 
         for bi in range(n_batches):
             src_start = bi*src_batch_size
@@ -181,22 +230,21 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
             src_slice = slice(src_start, src_end)
 
             # Create an event mask of shape (N_sources,N_events)
-            src_sin_alt_band_min = np.sin(np.deg2rad(np.rad2deg(alt) - self.band_deg_alt_range/2))
-            src_sin_alt_band_max = np.sin(np.deg2rad(np.rad2deg(alt) + self.band_deg_alt_range/2))
-
-            # Calculate the solid angle of the declination band.
-            src_alt_band_omega = (
-                2 * np.pi * (src_sin_alt_band_max - src_sin_alt_band_min)
-            )
-            
             ev_mask = np.logical_and(
                     (data_mc_sin_true_alt >=
                         src_sin_alt_band_min[src_slice][:, np.newaxis]),
                     (data_mc_sin_true_alt <=
                         src_sin_alt_band_max[src_slice][:, np.newaxis])
                 )
+            if self.energy_range is not None:
+                ev_mask &= np.logical_and(
+                    (data_mc_true_energy >= self.energy_range[0]),
+                    (data_mc_true_energy <= self.energy_range[1])
+                )
+
             print(src_sin_alt_band_min, src_sin_alt_band_max)
             print(np.count_nonzero(ev_mask))
+
             ev_idxs = np.tile(indices, bs)[ev_mask.ravel()]
             #print(ev_idxs)
             shg_src_idxs = bi*src_batch_size + np.repeat(
@@ -209,6 +257,9 @@ class PointLikeSourceSignalGenerationMethod(SignalGenerationMethod):
                     to_internal_flux_unit /
                     src_alt_band_omega[shg_src_idxs]
                 )
+            if src_weights is not None:
+                flux *= src_weights[shg_src_idxs]
+
             ev_idx_arr = np.append(ev_idx_arr, ev_idxs)
             shg_src_idx_arr = np.append(shg_src_idx_arr, shg_src_idxs)
             flux_arr = np.append(flux_arr, flux)
