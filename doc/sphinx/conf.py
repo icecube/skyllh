3dc97b0850058cc8c7dbfd85fe3514375bba0b2d
Tomas Kontrimas <t.kontrimas@outlook.com>
2026-04-22 18:23:36 +0200

    Update docs theme to `shibuya`

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -52,7 +52,6 @@ extensions = [
     'nbsphinx',
     'sphinxcontrib.apidoc',
     'sphinx_multiversion',
-    'sphinx_rtd_theme',
 ]
 
 # Extensions configuration.
@@ -132,102 +131,29 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'shibuya'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'accent_color': 'blue',
+    'github_url': 'https://github.com/icecube/skyllh',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['html_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-
-html_sidebars = {
-    '**': [
-        'versions.html',
-    ],
-}
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'SkyLLHdoc'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    'papersize': 'a4paper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'SkyLLH.tex', 'SkyLLH Documentation', 'The IceCube Collaboration', 'manual'),
-]
+def _smv_shibuya_compat(app, pagename, templatename, context, doctree):
+    current = context.get('current_version')
+    if hasattr(current, 'name'):
+        context['current_version'] = current.name
 
 
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, 'skyllh', 'SkyLLH Documentation', [author], 1)]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        'SkyLLH',
-        'SkyLLH Documentation',
-        author,
-        'SkyLLH',
-        'One line description of project.',
-        'Miscellaneous',
-    ),
-]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
+def setup(app):
+    app.connect('html-page-context', _smv_shibuya_compat, priority=600)
